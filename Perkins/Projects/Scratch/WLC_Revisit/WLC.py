72d1492e9eb947416bd55ddc4d14c58d5256b2bf
Your Name <patirck.heenan@colorado.edu>
2016-07-14 16:51:01 -0600

    using x vs F fitting seems to be *very* sucessful, at least for getting an initial guess.

--- Perkins/Projects/Scratch/WLC_Revisit/WLC.py
+++ Perkins/Projects/Scratch/WLC_Revisit/WLC.py
@@ -8,29 +8,51 @@ import sys
 sys.path.append("../../../../../")
 from FitUtil.WormLikeChain.Python.Code import WLC_Fit
 from GeneralUtil.python import PlotUtilities as pPlotUtil
-def Key(i,j):
-    return str(i) + str(j)
+
+def SafeCast(x):
+    if (type(x) is not int) and (type(x) is not float):
+        return np.array(list(x)).astype(np.complex128)
+    else:
+        return complex(x)
+
+def Power(x,y):
+    return np.power(SafeCast(x),SafeCast(y))
+
+def Complex(r,i):
+    return r + (1j * i)
+
+def Sqrt(x):
+    return np.sqrt(SafeCast(x))
+
+def ExtensionPerForce(kbT,Lp,L0,K0,F):
+    ToRet = L0*(F/K0 - (-9*kbT - 4*F*Lp)/(12.*kbT) + 
+     ((1 + Complex(0,1)*Sqrt(3))*(-9*Power(kbT,2) + 24*F*kbT*Lp - 
+          16*Power(F,2)*Power(Lp,2)))/
+      (24.*kbT*Power(-243*Power(kbT,3) + 108*F*Power(kbT,2)*Lp - 
+          144*Power(F,2)*kbT*Power(Lp,2) + 64*Power(F,3)*Power(Lp,3) + 
+          12*Sqrt(3)*Sqrt(135*Power(kbT,6) - 108*F*Power(kbT,5)*Lp + 
+             144*Power(F,2)*Power(kbT,4)*Power(Lp,2) - 
+             64*Power(F,3)*Power(kbT,3)*Power(Lp,3)),0.3333333333333333)) - 
+     ((1 - Complex(0,1)*Sqrt(3))*Power(-243*Power(kbT,3) + 108*F*Power(kbT,2)*Lp - 
+          144*Power(F,2)*kbT*Power(Lp,2) + 64*Power(F,3)*Power(Lp,3) + 
+          12*Sqrt(3)*Sqrt(135*Power(kbT,6) - 108*F*Power(kbT,5)*Lp + 
+             144*Power(F,2)*Power(kbT,4)*Power(Lp,2) - 
+             64*Power(F,3)*Power(kbT,3)*Power(Lp,3)),0.3333333333333333))/(24.*kbT))
+    # can have complex extension; we just want the real part. 
+    return np.real(ToRet)
+
+def run():
+    Force = np.linspace(0,250,1000) * 1e-12
+    Force += + np.random.rand(Force.size) * 10e-12
+    kbT = 4.1e-21
+    Lp = 10e-9
+    L0 = 650e-9
+    K0 = 1000e-12
+    ExtSixFifty = ExtensionPerForce(kbT,Lp,L0,K0,Force)
+    ExtFiveHundred = ExtensionPerForce(kbT,Lp,500e-9,K0,Force)
+    plt.plot(ExtSixFifty,Force)
+    plt.plot(ExtFiveHundred,Force,color='g',linestyle='--')
+    plt.show()
 
 if __name__ == "__main__":
-    base = "./Out/"
-    # only Lp/kbT matters, so just set kbT=1
-    kbT = 4.1e-21
-    N = 10
-    LpArr = np.linspace(1e-9,60e-9,num=N)
-    L0Arr = np.linspace(0.7,1.1,num=N)
-    K0 = 1000e-12
-    ext = np.linspace(0,1.3,num=500)
-    ForceArr = dict()
-    for i,Lp in enumerate(LpArr):
-        for j,L0 in enumerate(L0Arr):
-            Force = WLC_Fit.WlcExtensible(ext,kbT,Lp,L0,K0)
-            ForceArr[Key(i,j)] = Force.copy()
-    # POST: have all the forces, plot
-    for i,Lp in enumerate(LpArr):
-        for j,L0 in enumerate(L0Arr):
-            MyForce = ForceArr[Key(i,j)]
-            fig = pPlotUtil.figure()
-            plt.plot(ext,MyForce)
-            pPlotUtil.lazyLabel("Extension","Force","")
-            Id = "{:d}-{:d}_Lp={:.3g}_L0={:.3g}.png".format(i,j,Lp,L0)
-            pPlotUtil.savefig(fig,base + Id)
+    run()
