added in filtering to FEC, bug fies to zeroing for WLC retract portion, corrected is automagically offset and sign flipped now. WLC demo shows how Lp varies now, a little cleaner. minor plot changes for presentation

Perkins/AnalysisUtil/ForceExtensionAnalysis/FEC_Util.py
@@ -246,7 +246,7 @@ def GetFilteredForce(Obj,NFilterPoints):
     return ToRet
 
 def GetSurfaceIndexAndForce(TimeSepForceObj,Fraction,FilterPoints,
-                            ZeroAtStart=True):
+                            ZeroAtStart=True,FlipSign=True):
     """
     Given a retraction curve, a fraction of end-points to take the median of,
     and a filtering for the entire curve, determines the surface location
@@ -258,6 +258,9 @@ def GetSurfaceIndexAndForce(TimeSepForceObj,Fraction,FilterPoints,
         FilterPoints: see GetAroundTouchoff
         ZeroAtStart: if true, uses the first 'fraction' points; otherwise 
         uses the last 'fraction' points
+
+        FlipSign: if true (default), assumes the data is 'raw', so that
+        Dwell happens at positive force. Set to false if already fixed
     Returns: 
         Tuple of (Integer surface index,Zero Force)
     """
@@ -269,7 +272,10 @@ def GetSurfaceIndexAndForce(TimeSepForceObj,Fraction,FilterPoints,
     else:
         ForceFilter = o.Force
     # Flip the sign of the force
-    ForceSign = -1 * ForceFilter 
+    if (FlipSign):
+        ForceSign = -1 * ForceFilter
+    else:
+        ForceSign = ForceFilter
     N = ForceSign.size
     NumMed = int(N*Fraction)
     if (ZeroAtStart):
@@ -284,7 +290,7 @@ def GetSurfaceIndexAndForce(TimeSepForceObj,Fraction,FilterPoints,
     return ZeroIdx,MedRetr
 
 def GetFECPullingRegion(o,fraction=0.05,FilterPoints=20,
-                        MetersAfterTouchoff=None,Correct=False):
+                        MetersAfterTouchoff=None,Correct=False,**kwargs):
     """
     Args:
         fraction: Amount to average to determine the zero point for the force. 
@@ -293,9 +299,11 @@ def GetFECPullingRegion(o,fraction=0.05,FilterPoints=20,
         MetersFromTouchoff: gets this many meters away from the surface. If
         None, just returns all the data
         Correct: if true, corrects the data by flipping it and zeroing it out. 
+
+        **kwargs: passed on to GetSurfaceIndexAndForce
     """
     ZeroIdx,MedRetr =  GetSurfaceIndexAndForce(o,fraction,FilterPoints,
-                                               ZeroAtStart=False)
+                                               ZeroAtStart=False,**kwargs)
     if (MetersAfterTouchoff is not None):
         XToUse  = o.Separation
         N = XToUse.size
@@ -340,7 +348,7 @@ def GetAroundTouchoff(Objects,**kwargs):
         ToRet.append(GetFECPullingRegion(o,**kwargs))
     return ToRet
 
-def GetRegionForWLCFit(RetractOriginal,
+def GetRegionForWLCFit(RetractOriginal,SurfaceFilterFraction=0.02,
                        NFilterFraction=0.1,
                        NoTriggerDistance=150e-9):
     """
@@ -357,6 +365,11 @@ def GetRegionForWLCFit(RetractOriginal,
         TimeSepForce Object of the portion of the curve to fit 
     """
     Retract = copy.deepcopy(RetractOriginal)
+    # now, get just the 'post touchoff' region. We *dont* want to flip
+    # the sign when doing this
+    SurfaceNPoints = int(np.ceil(Retract.Force.size *SurfaceFilterFraction))
+    Retract = GetFECPullingRegion(Retract,FlipSign=False,
+                                  FilterPoints=SurfaceNPoints)
     N = Retract.Force.size
     NFilterPoints = int(np.ceil(NFilterFraction*N))
     RetractZeroSeparation = Retract.Separation
@@ -388,7 +401,7 @@ def GetRegionForWLCFit(RetractOriginal,
     # *dont* sign correct anything.
     NearSurface = GetFECPullingRegion(Retract,
                                       MetersAfterTouchoff=MetersAfterTouchoff,
-                                      Correct=False)
+                                      Correct=False,FlipSign=False)
     return NearSurface
 
     

Perkins/Projects/CircularDNA/2016-7-5-hot-deposition-analysis/MainCorrection.py
@@ -27,30 +27,33 @@ def GetCorrectedFECs(DataArray):
     Corrected = []
     for i,Tmp in enumerate(DataArray):
         CorrectionObj = CorrectionByFFT.CorrectionObject()
-        ApproachCorrected,RetractCorrected = \
-                CorrectionObj.CorrectApproachAndRetract(Tmp)
+        Approach,Retract = CorrectionObj.CorrectApproachAndRetract(Tmp)
+        # zero out the forces, pre-process...
+        ApproachCorrected,RetractCorrected =\
+            FEC_Util.PreProcessApproachAndRetract(Approach,Retract)
         Corrected.append([ApproachCorrected,RetractCorrected])
     return Corrected
 
 def GetWLCFits(CorrectedApproachAndRetracts):
     # get all the fits to the corrected WLCs
     ToRet = []
-    for Approach,Retract in CorrectedApproachAndRetracts:
-        _,RetractProcessed = FEC_Util.PreProcessApproachAndRetract(Approach,
-                                                                   Retract)
+    for i,(Approach,Retract) in enumerate(CorrectedApproachAndRetracts):
         # get the zerod and corrected Force extension curve
-        WLCFitFEC = FEC_Util.GetRegionForWLCFit(RetractProcessed)
+        WLCFitFEC = FEC_Util.GetRegionForWLCFit(Retract)
         # actually fit the WLC
-        Bounds = GetBoundsDict(**dict(Lp=[30e-9,55e-9],
+        Bounds = GetBoundsDict(**dict(Lp=[20e-9,200e-9],
                                       L0=[150e-9,700e-9],
                                       K0=[1000e-12,1400e-12],
                                       kbT=[0,np.inf]))
         SepNear = WLCFitFEC.Separation
         ForceNear = WLCFitFEC.Force
-        Fit = BoundedWlcFit(SepNear,ForceNear,VaryL0=True,VaryLp=True,Ns=40,
+        Fit = BoundedWlcFit(SepNear,ForceNear,VaryL0=True,VaryLp=True,Ns=20,
                             Bounds=Bounds)
         Pred = Fit.Predict(SepNear)
         ToRet.append([SepNear,Fit])
+        print("{:d}/{:d}".format(i,len(CorrectedApproachAndRetracts)))
+        print(Fit)
+        break
     return ToRet
 
 def run():
@@ -65,22 +68,20 @@ def run():
     for i,Name in enumerate(FullNames):
         DataArray.extend(pCheckUtil.getCheckpoint("Tmp{:d}.pkl".format(i),
                                                   ReadInData,False,Name))
-    # get all the corrected objects
+    # get all the corrected, force-zeroed objects
     Corrected= pCheckUtil.getCheckpoint("Corrected.pkl",GetCorrectedFECs,
                                         False,DataArray)
     # Get all the WLCs
     ListOfSepAndFits= pCheckUtil.getCheckpoint("WLC.pkl",GetWLCFits,
-                                               False,Corrected)
+                                               True,Corrected)
 
     # POST: have everything corrected, fit...
-    set_y_lim = lambda :  plt.ylim([-50,200])
+    set_y_lim = lambda :  plt.ylim([-50,100])
     set_x_lim = lambda :  plt.xlim([-10,1300])
     LegendOpts = dict(loc='upper left',frameon=True)
     # note: we want to pre-process (convert to sensible units etc) but no
     # need to correct (ie: flip and such)
-    PlotOptions = dict(FlipY=True,
-                       PreProcess=True,
-                       LegendOpts=LegendOpts)
+    PlotOptions = dict(LegendOpts=LegendOpts)
     for i,(ApproachCorrected,RetractCorrected) in enumerate(Corrected):
         SepNear,FitObj = ListOfSepAndFits[i]
         # get the WLC prediction
@@ -93,8 +94,12 @@ def run():
                                   **PlotOptions)
         plt.plot(WLC_Separation_nm,
                  WLC_Force_pN,linewidth=3,color='g',linestyle='--',
-                 label="WLC PRediction")
-        plt.axhline(65,label="65pN",linewidth=5.0,color='g',linestyle='--')
+                 label="WLC Prediction")
+        plt.axvline(650,label=r'L$_{\rm Contour}$=650nm',
+                    linewidth=5.0,color='g',linestyle='--')
+
+        plt.axhline(65,label=r'F$_{\rm Overstretch}$=65pN',
+                    linewidth=5.0,color='k',linestyle='-')
         set_y_lim()
         set_x_lim()
         pPlotUtil.legend(**LegendOpts)

Perkins/Projects/CircularDNA/2016_6_10_ContourLengthAnalysis/MainContourLength.py
@@ -105,18 +105,18 @@ def run():
         # and as integers (ie: dont care about 2%
         L0_nm = int(ToNm(Fit.Info.ParamVals.ParamDict["L0"].Value))
         Lp_nm = int(ToNm(Fit.Info.ParamVals.ParamDict["Lp"].Value))
-        plt.axvline(ToNm(MetersAfterTouchoff))
         # plot the WLC prediction, label...
+        """
         plt.plot(ToNm(SepNear),ToPn(Pred),color='g',linestyle='--',
                  linewidth=5.0,
-                 label="WLC (Extensible)\n" +\
-                 r"$L_0$={:d}nm, $L_p$={:d}nm".format(L0_nm,Lp_nm))
+                 label="WLC (Extensible)\n")
+        """
         pPlotUtil.legend(frameon=True)
         # note: limits are in nm and pN
         MaxY_pN = np.max(ToPn(Pred[np.where(np.isfinite(Pred))]))
         MaxY_pN = max(MaxY_pN,ToPn(np.max(Retr.Force)))
         MinY_pN = -MaxY_pN/5
-        plt.ylim([MinY_pN,MaxY_pN])
+        plt.ylim([-40,MaxY_pN])
         plt.xlim([-20,plt.xlim()[-1]])
         Name = "WLC" + Tmp.Meta.Name
         pPlotUtil.savefig(fig,Name + ".png")

Perkins/Projects/Scratch/WLC_Revisit/WLC.py
@@ -6,52 +6,34 @@ import matplotlib.pyplot as plt
 import sys
 
 sys.path.append("../../../../../")
-from FitUtil.WormLikeChain.Python.Code import WLC_Fit
+from FitUtil.WormLikeChain.Python.Code import WLC_ComplexValued_Fit 
+
+
 from GeneralUtil.python import PlotUtilities as pPlotUtil
 
-def SafeCast(x):
-    if (type(x) is not int) and (type(x) is not float):
-        return np.array(list(x)).astype(np.complex128)
-    else:
-        return complex(x)
-
-def Power(x,y):
-    return np.power(SafeCast(x),SafeCast(y))
-
-def Complex(r,i):
-    return r + (1j * i)
-
-def Sqrt(x):
-    return np.sqrt(SafeCast(x))
-
-def ExtensionPerForce(kbT,Lp,L0,K0,F):
-    ToRet = L0*(F/K0 - (-9*kbT - 4*F*Lp)/(12.*kbT) + 
-     ((1 + Complex(0,1)*Sqrt(3))*(-9*Power(kbT,2) + 24*F*kbT*Lp - 
-          16*Power(F,2)*Power(Lp,2)))/
-      (24.*kbT*Power(-243*Power(kbT,3) + 108*F*Power(kbT,2)*Lp - 
-          144*Power(F,2)*kbT*Power(Lp,2) + 64*Power(F,3)*Power(Lp,3) + 
-          12*Sqrt(3)*Sqrt(135*Power(kbT,6) - 108*F*Power(kbT,5)*Lp + 
-             144*Power(F,2)*Power(kbT,4)*Power(Lp,2) - 
-             64*Power(F,3)*Power(kbT,3)*Power(Lp,3)),0.3333333333333333)) - 
-     ((1 - Complex(0,1)*Sqrt(3))*Power(-243*Power(kbT,3) + 108*F*Power(kbT,2)*Lp - 
-          144*Power(F,2)*kbT*Power(Lp,2) + 64*Power(F,3)*Power(Lp,3) + 
-          12*Sqrt(3)*Sqrt(135*Power(kbT,6) - 108*F*Power(kbT,5)*Lp + 
-             144*Power(F,2)*Power(kbT,4)*Power(Lp,2) - 
-             64*Power(F,3)*Power(kbT,3)*Power(Lp,3)),0.3333333333333333))/(24.*kbT))
-    # can have complex extension; we just want the real part. 
-    return np.real(ToRet)
 
 def run():
-    Force = np.linspace(0,250,1000) * 1e-12
-    Force += + np.random.rand(Force.size) * 10e-12
+    ForceInitial = np.linspace(0,250,1000) * 1e-12
     kbT = 4.1e-21
     Lp = 10e-9
     L0 = 650e-9
     K0 = 1000e-12
-    ExtSixFifty = ExtensionPerForce(kbT,Lp,L0,K0,Force)
-    ExtFiveHundred = ExtensionPerForce(kbT,Lp,500e-9,K0,Force)
-    plt.plot(ExtSixFifty,Force)
-    plt.plot(ExtFiveHundred,Force,color='g',linestyle='--')
+    LpFactors = [1,1.2,2,5,10,20,50,100]
+    Extensions = []
+    Forces = []
+    for Factor in LpFactors:
+        Args = (kbT,Lp*Factor,L0,K0,ForceInitial)
+        ExtensionTmp = WLC_ComplexValued_Fit.ExtensionPerForce(*Args)
+        FinalForce = WLC_ComplexValued_Fit.InvertedWlcForce(ExtensionTmp,*Args)
+        Forces.append(FinalForce)
+        Extensions.append(ExtensionTmp)
+    # now plot everything
+    XPlotFunc = lambda x: x*1e9
+    YPlotFunc = lambda y: y*1e12
+    for i,(Ext,Force) in enumerate(zip(Extensions,Forces)):
+        plt.plot(XPlotFunc(Ext),YPlotFunc(Force),
+                 label="Lp~{:.2f}".format(LpFactors[i]))
+    pPlotUtil.lazyLabel("Extension [nm]","Force [pN]","")
     plt.show()
 
 if __name__ == "__main__":

Perkins/Projects/WetLab/Demos/Prep_Optimizations/5_26_2016_Ovh2.0_Labelling/Main_ovh2.0_Labelling.py
@@ -108,8 +108,8 @@ def run():
     # N/10 (meaning 10% efficiency), then the mean distance increases by
     # sqrt(10)~3
     LowerEfficiency= MeanDistNano*np.sqrt(1/efficiency)
-    LowerBoundDist = 5*RadiusOfGyrLinearNanoMeters
-    UpperBoundDist = 15*RadiusOfGyrLinearNanoMeters
+    LowerBoundDist = 2.5*DNASizeNanoMeters
+    UpperBoundDist = 8*DNASizeNanoMeters
     plt.loglog(LoadConcentrationNgPerUl,LowerEfficiency,
                'm.-',linewidth=3,
                label="{:d}% efficiency".format(int(efficiency*100)))
@@ -130,7 +130,7 @@ def run():
     # bit of a hack to get the label working
     pPlotUtil.lazyLabel("Concentration [ng/uL] (20uL Deposition)",
                         "Expected Mean DNA distance [nm]",\
-                    "Deposit 20uL at a concentration of 0.2 ng/uL to 1ng/uL",
+                        "Tuning Deposition Concentration to avoid dimerization",
                         titley=1.1)
     # our circular DNA is roughly 607nm
     plt.axhline(DNASizeNanoMeters,linewidth=4,linestyle='--',
@@ -141,7 +141,7 @@ def run():
                          secondY=False)
     plt.xlabel("Molarity (nM)")
     plt.tight_layout()
-    fig.savefig("DepositionAdvice.png")
+    pPlotUtil.savefig(fig,"DepositionAdvice.png")
     """
     We load the 20uL into xuL of TE and heat; what volume should we use?
     """
@@ -209,7 +209,6 @@ def run():
     "Radius of Gyration of Plasmid DNA Isoforms from Static Light Scattering." 
     Biotechnology and Bioengineering 107, 2010
     """
-    
     BasePairs = np.logspace(2.3,3.5)
     LengthsNm = BasePairs * NanometersPerBp
     # get the plot the DNA radii of gyration
@@ -232,7 +231,16 @@ def run():
              label="Circular WLC: {:d}nm".\
              format(int(CircularSequenceRadiusCirc)),
              **StyleCirc)
-    plt.axvline(NumBasePairsCirc,color='g',linewidth=2)
+    plt.axvline(NumBasePairsCirc,color='g',
+                label="200bp, R={:d}".format(int(CircularSequenceRadiusCirc)),
+                                             linewidth=2)
+    LongerConstructBp = 1800
+    LongerConstructNm = NanometersPerBp*LongerConstructBp
+    LongerConstructCircularRadiusNm = \
+        int(CircularRadiusOfGyration(Lp_nm,LongerConstructNm))
+    plt.axvline(LongerConstructBp,color='r',linestyle='--',
+                label="1800bp, R={:d}".format(LongerConstructCircularRadiusNm),
+                linewidth=2)
     XStart = plt.xlim()[0]
     ArrX = [XStart,NumBasePairsCirc]
     ArrY1 = [CircularSequenceRadiusCirc,CircularSequenceRadiusCirc]
@@ -240,7 +248,7 @@ def run():
     plt.plot(ArrX,ArrY1,linestyle="--",**StyleCirc)
     plt.plot(ArrX,ArrY2,linestyle="--",**StyleLin)
     pPlotUtil.lazyLabel("Base Pairs","Radius of Gyration (nm)",
-                        "Circular DNA construct radius of gyration < 20nm",
+    "Circular radius of gyration converges to linear for small constructs",
                         frameon=True)
     pPlotUtil.savefig(fig,"Gyration.png")
 