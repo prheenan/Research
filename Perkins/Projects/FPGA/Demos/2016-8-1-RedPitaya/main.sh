0bd2f57d60e4379136eb458f81a59070fca435fb
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2016-08-02 14:12:47 -0600

    minor changes to main

--- Perkins/Projects/FPGA/Demos/2016-8-1-RedPitaya/main.sh
+++ Perkins/Projects/FPGA/Demos/2016-8-1-RedPitaya/main.sh
@@ -6,7 +6,7 @@
 # $1: Build directory. Default, assumes home
 # $2: Vivado Binary Path, assumes 2016.2 by default
 WorkingDir=${1:-~}
-VivadoBin=${2:-"/opt/Xilinx/Vivado/2016.2/bin/"}
+VivadoBin=${2:-/opt/Xilinx/Vivado/2016.2/bin/}
 #  Currently, the configuration file assumes that 'patrick' and 'perkins'
 # are the two users needed. If you have a different username, add it to the
 # config file "schroot_config" to the relevant csv-separated lists
@@ -19,8 +19,10 @@ sudo apt-get install make curl xz-utils
 sudo apt-get install libssl-dev device-tree-compiler u-boot-tools
 #Vivado requires a gmake executable which does not exist on Ubuntu. It is necessary to create a symbolic link to the regular make executable.
 sudo ln -s /usr/bin/make /usr/bin/gmake
+# download schroot
+sudo apt-get install schroot
 # remember where we ran this from, to get the config file
-Dir=$PWD
+Dir=`pwd`
 # build in the home directory
 cd $WorkingDir
 git clone https://github.com/RedPitaya/RedPitaya.git 
