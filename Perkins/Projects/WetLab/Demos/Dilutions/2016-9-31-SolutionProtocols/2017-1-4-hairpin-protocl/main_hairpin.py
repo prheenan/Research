dd2249ecf8045836ff3b3d543b853e31c1b56163
Your Name <patirck.heenan@colorado.edu>
2017-01-11 16:39:44 -0700

    small changes to hairpin-relaed stuff. Upped the concentration consierable for the hairpin itself.

--- Perkins/Projects/WetLab/Demos/Dilutions/2016-9-31-SolutionProtocols/2017-1-4-hairpin-protocl/main_hairpin.py
+++ Perkins/Projects/WetLab/Demos/Dilutions/2016-9-31-SolutionProtocols/2017-1-4-hairpin-protocl/main_hairpin.py
@@ -15,9 +15,9 @@ def run():
     """
     # to get the initial concentrations we want, need to dilute from 
     # 10uM to something like 1nM
-    stock_conc = 100
-    desired_conc = [10,1,1e-1,1e-2]
-    volumes = [60]
+    stock_conc = 10
+    desired_conc = [1,2e-1]
+    volumes = [10,10,300]
     print("First, dilute hairpin stock to get 'x' concentration ({:.2f}uM)".\
           format(min(desired_conc)))
     DilutionUtil.PrintSerialSteps(stock_conc,volumes,sorted(desired_conc)[::-1],
@@ -27,9 +27,9 @@ def run():
     # to get all the concentrations we want, need 
     # (1) same volume of hairpin
     # (2) varying volumes of buffer and TE as appropriate...
-    # note that this reduces the DNA concentration to about 90nM
-    stock_conc_units_of_min = 1e4
-    desired_conc = [1e3,3e2,1e2,3e1,1e1,1]
+    # the stock concentration of complement is at 100x.
+    stock_conc_units_of_min = 100/min(desired_conc)
+    desired_conc = [3e2,1e2,3e1,1e1,1]
     volumes = [10]
     DilutionUtil.PrintSerialSteps(stock_conc_units_of_min,
                                   volumes,sorted(desired_conc)[::-1],
--- Perkins/Projects/WetLab/Demos/Gel_Standard/2017-1-4-gel_%_recommendations/main_gel_recommendations.py
+++ Perkins/Projects/WetLab/Demos/Gel_Standard/2017-1-4-gel_%_recommendations/main_gel_recommendations.py
@@ -29,14 +29,17 @@ def run():
     the bromophenol blue goes with 45bp (ie: probably want to stop when it is
     <= 1/2 for our ~30bp) 
 
-    
+    See also: 
+
+    Ausubel, F. M. Current protocols in molecular biology, 1987
     """
     percentage_and_range = [ [0.5,1000,30000],
                              [0.7,800,12000],
                              [1  ,500,10000],
                              [1.2,400,7000],
                              [1.5,200,3000],
-                             [2.0,50 ,2000]]
+                             [2.0,50 ,2000],
+                             [3.0,10,1000]]
     pct = [p[0] for p in percentage_and_range ]
     bp_low = [p[1] for p in percentage_and_range ]
     bp_high = [p[2] for p in percentage_and_range ]
@@ -45,7 +48,9 @@ def run():
     coeffs_high = np.polyfit(x=pct,y=np.log(bp_high),deg=1)
     desired_pct = 2.5
     desired_bp_resolution = 20
-    xvals = np.linspace(0,desired_pct)
+    max_pct = max(pct)
+    max_graph = max(max_pct,desired_pct)
+    xvals = np.linspace(0,max_graph)
     # get the fits
     fit_low = np.exp(np.polyval(coeffs_low,xvals))
     fit_high = np.exp(np.polyval(coeffs_high,xvals))
@@ -66,14 +71,43 @@ def run():
     plt.fill_between(xvals,fit_low,fit_high,**fill_style)
     plt.plot([],[],label="resolvable region",linewidth=20,**fill_style)
     plt.ylim(desired_bp_resolution/2,max(bp_high)*2)
-    plt.xlim(0,desired_pct * 1.1)
+    plt.xlim(0,max_graph * 1.1)
     plt.yscale('log')
     title = "Expect resolution between {:d}bp and {:d}bp at {:.1f}% Agarose".\
             format(int(expected_at_desired_low),int(expected_at_desired_high),
                    desired_pct)
     xlabel = r"Agarose percentage ($\frac{\mathrm{g}}{\mathrm{mL}}$)"
     PlotUtilities.lazyLabel(xlabel,"Resolvable bp",title,frameon=True)
-    PlotUtilities.savefig(fig,"./out.png")
+    PlotUtilities.savefig(fig,"./out_agarose.png")
+    """
+    # seems a bit sketchy, but here is some data on 
+    http://openwetware.org/wiki/Bromophenol_blue
+    1000bp at 0.7% agarose
+    500bp at 1%
+    150bp at 2%
+    50bp at 3% 
+    """
+    pct_bp = [ [0.7,1000],
+               [1  ,500 ],
+               [2  ,150 ],
+               [3  ,50  ]]
+    pct = [p[0] for p in pct_bp]
+    bp = [p[1] for p in pct_bp]
+    lin_coeffs_dye = np.polyfit(pct,np.log(bp),deg=1)
+    x_fit = np.linspace(min(pct),max(pct))
+    fit_dye = np.exp(np.polyval(lin_coeffs_dye,x_fit))
+    expected_running = np.exp(np.polyval(lin_coeffs_dye,desired_pct))
+    fig = PlotUtilities.figure()
+    plt.plot(pct,bp,'ro-')
+    plt.plot(x_fit,fit_dye,'b--')
+    plt.plot(desired_pct,expected_running,'ko',markersize=10)
+    title = "Effective size (in bp) of Bromophenol blue ({:d} at {:.1f}%)".\
+            format(int(expected_running),desired_pct)
+    PlotUtilities.lazyLabel("percentage","Effective dye size (bp)",title)
+    plt.yscale('log')
+    plt.ylim(min(bp)/2,max(bp)*2)
+    plt.xlim(0,max_graph*1.1)
+    PlotUtilities.savefig(fig,"./out_dye.png")
 
 if __name__ == "__main__":
     run()
