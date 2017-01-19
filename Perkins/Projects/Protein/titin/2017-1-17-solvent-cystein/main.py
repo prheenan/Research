eb18ef0d2dfb2ef3b4a51c76ad6ee96737d6f33a
Your Name <patirck.heenan@colorado.edu>
2017-01-19 12:14:03 -0700

    areas now chain/residue/name specific (I think this is all we need). better commenting, better coloring (thiols are yellow). removed print

--- Perkins/Projects/Protein/titin/2017-1-17-solvent-cystein/main.py
+++ Perkins/Projects/Protein/titin/2017-1-17-solvent-cystein/main.py
@@ -27,12 +27,23 @@ def GetArea(identifier,solvent_area,dot_density=4,solvent_radius=1.4):
     cmd.set('solvent_radius', solvent_radius)
     return cmd.get_area(identifier)
 
-def GetAllAreas(name_cystines):
+def GetAllAreas(chains,residues,names):
+    """
+    gets the area of each individual atom in the residue and chain
+
+    Args:
+        chain: which chain to use
+        residues: numbers for the residues
+        names: name of the atom within a given residue
+    Returns:
+        surface area and solvent area for the given residues 
+    """
     # loop through, get the areas and accessible areas
     sasa_per_residue,area_per_residue = [],[]
-    for i in stored.residues:
-        solvent_area = GetArea("resi %s" % i,solvent_area=True)
-        molecular_area = GetArea("resi %s" % i,solvent_area=False)
+    for chain,residue,name in zip(chains,residues,names):
+        my_str = "chain {:s} & resi {:s} & name {:s}".format(chain,residue,name)
+        solvent_area = GetArea(my_str,solvent_area=True)
+        molecular_area = GetArea(my_str,solvent_area=False)
         sasa_per_residue.append(solvent_area)
         area_per_residue.append(molecular_area)
     return sasa_per_residue,area_per_residue
@@ -44,41 +55,82 @@ def run():
     cmd.bg_color('grey80')
     base="/Users/patrickheenan/src_prh/Research/Perkins/Projects/Protein/" +\
         "titin/2017-1-17-solvent-cystein/"
-    cmd.load(base +'1waa.pdb')  # use the name of your pdb file
+    """
+    Note: lyle sent me chain A (I think) of titin IG27
+    """
+    cmd.load(base +'1waa.pdb')
     maximum_accessible_area_angstroms_squared = 3
-    stored.residues = []
+    stored.residues,stored.names,stored.chains = [],[],[]
     # get all the cystines, only really care about them
     name_cystines = "cys"
     cmd.select(name_cystines,"resn " + name_cystines)
+    cmd.iterate(name_cystines, 'stored.chains.append(chain)')
     cmd.iterate(name_cystines, 'stored.residues.append(resi)')
+    cmd.iterate(name_cystines, 'stored.names.append(name)')
     ids = [float(r) for r in stored.residues]
     sasa_per_residue,area_per_residue= \
         CheckpointUtilities.getCheckpoint(base + "out.pkl",
-                                          GetAllAreas,False,name_cystines)
+                                          GetAllAreas,False,
+                                          stored.chains,stored.residues,
+                                          stored.names)
     accessible_tmp = np.where(sasa_per_residue > 
                               maximum_accessible_area_angstroms_squared)[0]
     ids_greater_than = [stored.residues[i] for i in accessible_tmp]
     # make all the cystines spheres
     cmd.show("spheres",name_cystines)
-    accessible_name = "accessible_name"
+    cmd.color("green",name_cystines)
+    accessible_name = "accessible_cys"
     # color the accessible ones black
     cmd.select(accessible_name,"resi " + "+".join(ids_greater_than))
-    cmd.color("black",accessible_name)
+    cmd.color("red",accessible_name)
+    thiol_name = "thiol"
+    cmd.select(thiol_name, "resn cys & name SG")
+    cmd.show("spheres","thiol")
+    cmd.color("yellow","thiol")
     # get the names of all the accessible ones...
-    stored.accessible_names, stored.accessible_chains = [],[]
-    cmd.iterate(name_cystines, 'stored.accessible_names.append(resn)')
-    cmd.iterate(name_cystines, 'stored.accessible_chains.append(chain)')
+    stored.cys_residue_names, stored.cys_atomic_names,stored.cys_chains = \
+        [],[],[]
+    cmd.iterate(name_cystines, 'stored.cys_residue_names.append(resn)')
+    cmd.iterate(name_cystines, 'stored.cys_chains.append(chain)')
+    cmd.iterate(name_cystines, 'stored.cys_atomic_names.append(name)')
+    """
+    The names property in pymol is a
+    "list of up to 4-letter codes for atoms in proteins or nucleic acids"
+    (see http://pymol.sourceforge.net/newman/user/S0220commands.html)
+
+    The code is available also here: 
+    http://chemistry.umeche.maine.edu/Modeling/NAMD8.html
+
+    more details here: 
+    PROTEIN DATA BANK, 
+    ATOMIC COORDINATE AND BIBLIOGRAPHIC ENTRY FORMAT DESCRIPTION, 1992
+    cdn.rcsb.org/rcsb-pdb//file_formats/pdb/pdbguide2.2/PDB_format_1992.pdf
+
+    (1) greek letters are what you would think
+    (2) Cystine goes like:
+    
+    C_alpha -- C_beta -- S_gamma
+
+    or, in PDB notation:
+
+    CA -- CB -- SG
+    (see Table III, a of:
+    Abbreviations and Symbols for the Description of the Conformation of 
+    Polypeptide Chains. European Journal of Biochemistry 17, 193-201 (1970).)
+
+    too bad, looks like the thiol (S_gamma) is accessible)
+    """
     fraction =np.array(sasa_per_residue)/np.array(area_per_residue)
     # save out the data...
-    header_data = [ ["chain",stored.accessible_chains],
-                    ["residue name" ,stored.accessible_names],
+    header_data = [ ["chain",stored.cys_chains],
+                    ["residue name" ,stored.cys_residue_names],
                     ["residue number",ids],
+                    ["atom name",stored.cys_atomic_names],
                     ["accessible area (AA^2)",sasa_per_residue],
                     ["total residue area (AA^2)",area_per_residue],
                     ["fraction accessible",fraction]]
     header = ",".join([h[0] for h in header_data])
     data = np.array([h[1] for h in header_data],dtype=np.object).T
-    print(data)
     np.savetxt(fname=base + "out.csv",X=data,fmt="%s",delimiter=",",
                header=header)
 
