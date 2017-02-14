71156d79a9fb252d099a55eb1c7efa15bb49ea2c
patrick heenan <patrick.heenan@colorado.edu>
2017-02-14 10:25:12 -0700

    better lipid FEC, csv working on windows now (using network path function in FEC_Util). slow as all hell...

--- Perkins/Projects/Lipids/2017-2-13-postive-control-gallery/main_positive.py
+++ Perkins/Projects/Lipids/2017-2-13-postive-control-gallery/main_positive.py
@@ -25,23 +25,26 @@ def run():
             "PositiveControls/SingleAttachments/"
     out = base + "RepresentativeGallery/"
     threshold_meters = 10e-9
+    adhesion_meters = 5e-9
     files,raw_data = FEC_Util.read_and_cache_pxp(base,force=False)
     proccess_opts = dict(zero_separation_at_zero_force=True)
     processed = [FEC_Util.SplitAndProcess(r,**proccess_opts) for r in raw_data]
     retr_filtered = []
     for i,(appr,retr) in enumerate(processed):
+        adhesion_idx = np.where(retr.Separation < adhesion_meters)[0]
+        after_adhesion_idx = adhesion_idx[-1] + 1
         # only look where the retract max makes sense XXX should filter pxp
-        arg_max = np.argmax(retr.Force)
-        if (retr.Separation[arg_max] > threshold_meters):
+        arg_max = np.argmax(retr.Force[after_adhesion_idx:])
+        if (retr.Separation[after_adhesion_idx:][arg_max] > threshold_meters):
             continue
         # POST: separation is some kind of sensible (less than threshold_meters)
         fig = PlotUtilities.figure()
-        FEC_Plot.FEC_AlreadySplit(appr,retr)
+        FEC_Plot.FEC_AlreadySplit(appr,retr,NFilterPoints=250)
         # get some sensible limit in nm/pN for x/y
         min_x = plt.xlim()[0]
         max_y = plt.ylim()[-1]
         max_x = min_x + 40
-        plt.xlim([min_x,max_x])
+        plt.xlim([-5,max_x])
         plt.ylim([-30,max_y])
         PlotUtilities.savefig(fig,out+ "FEC_{:d}.pdf".format(i))
         # filter the retract to just the bits we care about
@@ -51,6 +54,7 @@ def run():
     # make a 2-D histogram of the fitered ones 
     fig = PlotUtilities.figure()
     FEC_Plot.heat_map_fec(retr_filtered)
+    plt.ylim(-30,plt.ylim()[1])
     PlotUtilities.savefig(fig,out + "histogram.pdf")
 if __name__ == "__main__":
     run()
