86e4359c78e621dccdba1136dc77cb6b3b28e46c
Your Name <patirck.heenan@colorado.edu>
2017-02-20 15:34:18 -0700

    added creation of table for primer

--- Personal/EventDetection/OtherMethods/demo_my_method/main_minimal_working_example.py
+++ Personal/EventDetection/OtherMethods/demo_my_method/main_minimal_working_example.py
@@ -23,7 +23,8 @@ def local_stdev(f,n):
         array, same size as f, with the dat we want
     """
     max_n = f.size
-    return [np.std(f[max(0,i-n):min(max_n,i+n)]) for i in range(max_n)]
+    return np.array([np.std(f[max(0,i-n):min(max_n,i+n)]) 
+                     for i in range(max_n)])
 
 def run():
     """
@@ -39,23 +40,31 @@ def run():
     retract = ex.retract
     time,separation,force = retract.Time,retract.Separation,retract.Force
     n_points = ex.tau_num_points
-    interp = ex.retract_spline_interpolator(deg=2)
+    # N degree b-spline has continuous (N-1) derivative
+    interp = ex.retract_spline_interpolator(deg=3)
+    interp_second = interp.derivative(2)
+    # get the interpolated derivative
     interpolated_force = interp(time)
     event_slices = ex.get_retract_event_idx()
     # get a model for the local standard deviaiton using the autocorrelation
     # time from the event
     diff = force-interpolated_force
     stdevs = local_stdev(diff,n_points)
+    global_stdev = np.std(diff)
+    median_local_stdev = np.median(stdevs)
+    thresh = median_local_stdev*1.1
     # plot everything
     style_events = dict(color='r',label="True events")
+    time_limits = [min(time),max(time)]
     fig = PlotUtilities.figure()
-    plt.subplot(2,1,1)
+    plt.subplot(3,1,1)
     plt.plot(time,force,color='k',alpha=0.3)
     plt.plot(time,interpolated_force,color='b',linewidth=2)
     Plotting.highlight_events(event_slices,time,force,
                               **style_events)
     PlotUtilities.lazyLabel("","Force (au)","")
-    plt.subplot(2,1,2)
+    plt.xlim(time_limits)
+    plt.subplot(3,1,2)
     plt.plot(time,stdevs)
     # plot the autocorrelation time along the plot
     min_x_auto = min(time) * 1.1
@@ -64,7 +73,26 @@ def run():
              linewidth=5,color='g',label="autocorrelation time")
     Plotting.highlight_events(event_slices,time,stdevs,linewidth=5,
                               **style_events)
-    PlotUtilities.lazyLabel("Time (au)","Event Stdev","")
+    PlotUtilities.lazyLabel("a","Local Stdev","")
+    plt.axhline( thresh,label="threshold",linestyle='--',color='r')
+    plt.xlim(time_limits)
+    plt.subplot(3,1,3)
+    mask = np.where(stdevs >  thresh)[0]
+    # XXX check mask has at least one...
+    print(mask)
+    peak_starts_after_first = mask[np.where(np.diff(mask) > 1)]
+    peak_starts = [mask[0]]
+    peak_starts.extend(peak_starts_after_first)
+    peak_ends = list(peak_starts_after_first-1)
+    peak_ends.extend(mask[-1])
+    print(peak_starts)
+    print(peak_ends)
+    event_centers = [np.mean([start,end]) 
+                     for start,end in zip(peak_starts,peak_ends)]
+    plt.plot(time,stdevs,'b.')
+    Plotting.highlight_events(event_centers,time,stdevs,linewidth=5,
+                              **style_events)
+    plt.xlim(time_limits)
     PlotUtilities.savefig(fig,"./out.png")
 
 if __name__ == "__main__":
