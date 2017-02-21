40c1e72268a2e50c4b72e633a7e22c6f4e749825
Your Name <patirck.heenan@colorado.edu>
2017-02-21 15:21:13 -0700

    switched to plotting just x...

--- Personal/EventDetection/OtherMethods/demo_my_method/main_minimal_working_example.py
+++ Personal/EventDetection/OtherMethods/demo_my_method/main_minimal_working_example.py
@@ -147,39 +147,40 @@ def run():
     stdevs = info.local_stdev
     # plot everything
     style_events = dict(color='r',label="True events")
-    time_limits = [min(time),max(time)]
     fig = PlotUtilities.figure()
-    n_plots = 4
+    n_plots = 3
+    x = separation
+    min_x,max_x = min(x),max(x)
+    x_range = max_x - min_x
+    fudge = x_range * 0.05
+    x_limits = [min_x - fudge,max_x + fudge]
+    x_label = "Time"
     plt.subplot(n_plots,1,1)
-    plt.plot(time,force,color='k',alpha=0.3)
-    plt.plot(time,interpolated_force,color='b',linewidth=2)
+    plt.plot(x,force,color='k',alpha=0.3)
+    plt.plot(x,interpolated_force,color='b',linewidth=2)
     Plotting.highlight_events(event_slices,time,force,
                               **style_events)
     PlotUtilities.lazyLabel("","Force (au)","")
-    plt.xlim(time_limits)
+    plt.xlim(x_limits)
     plt.subplot(n_plots,1,2)
-    plt.plot(time,stdevs)
     # plot the autocorrelation time along the plot
-    min_x_auto = min(time) * 1.1
+    min_x_auto = min(x) * 1.1
     auto_correlation_x = [min_x_auto,min_x_auto+ex.tau]
-    plt.semilogy(time[info.slice_fit],info.cdf)
-    Plotting.highlight_events(event_slices,time,info.cdf,linewidth=5,
+    plt.semilogy(x[info.slice_fit],info.cdf)
+    Plotting.highlight_events(event_slices,x,info.cdf,linewidth=5,
                               **style_events)
-    PlotUtilities.lazyLabel("","CDF ","")
     plt.axhline(thresh,label="threshold",linestyle='--',color='r')
-    plt.xlim(time_limits)
+    PlotUtilities.lazyLabel("","CDF ","")
+    plt.xlim(x_limits)
     plt.subplot(n_plots,1,3)
     # XXX check mask has at least one...
-    plt.plot(time,stdevs,'b.')
+    plt.plot(x,force,'b-',color='k',alpha=0.3)
     for fwd,rev,event in zip(event_idx_end,event_idx_start,event_idx):
-        plt.axvline(time[fwd],linestyle='--')
-        plt.axvline(time[rev])
-        plt.axvline(time[event],linewidth=3)
-    plt.xlim(time_limits)
-    PlotUtilities.lazyLabel("","","")
-    plt.subplot(n_plots,1,4)
-    plt.plot(time[mask],interp_first_deriv[mask],'b.')
-    plt.xlim(time_limits)
+        plt.axvline(x[fwd],linestyle='--',color='r')
+        plt.axvline(x[rev],color='g')
+        plt.axvline(x[event],linewidth=3)
+    plt.xlim(x_limits)
+    PlotUtilities.lazyLabel(x_label,"Force","")
     PlotUtilities.savefig(fig,"./out.png")
 
 if __name__ == "__main__":
