f1b1e8bb569465a5dfe5b51cf3cf33296344613b
patrick heenan <patrick.heenan@colorado.edu>
2017-02-24 08:50:07 -0700

    moved adhesion mask stuff to detector.

--- Personal/EventDetection/_2SplineEventDetector/Detector.py
+++ Personal/EventDetection/_2SplineEventDetector/Detector.py
@@ -23,6 +23,51 @@ def local_stdev(f,n):
     return np.array([np.std(f[max(0,i-n):min(max_n,i+n)]) 
                      for i in range(max_n)])
 
+def adhesion_function_for_split_fec(split_fec):
+    """
+    returns the funciton adhesion_mask, with surface_index set to whatever
+    the surface index of split_fec is predicted to be by the approach
+    
+    Args:
+        split_fec: the split_force_extension object we want to mask the 
+        adhesions of 
+    Returns:
+        lambda call to adhesion_mask, with surface_index already supplied
+    """
+    surface_index = split_fec.get_predicted_retract_surface_index()
+    return (lambda *args,**kwargs: adhesion_mask(surface_index,*args,**kwargs))
+    
+def adhesion_mask(surface_index,probability_distribution,threshold):
+    """
+    returns a boolean mask which is 0 where we can predict and adhesion 
+    and zero elsewhere
+    
+    Args:
+        surface_index: our best guess for where the surface is. 
+        probability_distribution: see Detector._event_mask
+        threshold: see Detector._event_mask
+    Returns:
+        list of event slices
+    """
+    to_ret = np.ones(probability_distribution.size,dtype=np.bool_)
+    non_events = probability_distribution > threshold
+    # remove all things before the predicted surface
+    to_ret[:surface_index] = 0
+    # remove everything until we arent at an event anymore
+    non_events_after_predicted_surface = np.where(non_events[surface_index:])[0]
+    if (non_events_after_predicted_surface.size == 0):
+        # everything is always an event after the surface. Not much we can do,
+        # so dont mess with to_ret
+        pass
+    else:
+        # zero out everything until the first non-event
+        first_non_event = non_events_after_predicted_surface[0]
+        absolute_index_of_non_event = surface_index + first_non_event
+        # XXX add in minimum distance between?
+        # XXX add in 'need to not have an event for a certain amount of time'?
+        to_ret[:absolute_index_of_non_event] = 0
+    return to_ret                     
+                     
 class prediction_info:
     def __init__(self,event_idx,event_slices,local_stdev,interp,mask,
                  cdf,slice_fit,threshold):
