cd234b4d936bdb03399049416e1ebb9485c3c752
patrick heenan <patrick.heenan@colorado.edu>
2017-02-27 10:19:27 -0700

    added in shuffling and validation, going to try running on larger set (N_=90, 30 each)

--- Personal/EventDetection/Util/Learning.py
+++ Personal/EventDetection/Util/Learning.py
@@ -45,10 +45,13 @@ class learning_curve:
         """
         self.description = description
         self.list_of_folds = None
+        self.validation_folds = None
         self.list_of_params = list_of_params
         self.func_to_call = func_to_call
     def set_list_of_folds(self,folds):
         self.list_of_folds = folds
+    def set_validation_folds(self,folds):
+        self.validation_folds = folds
     def _concatenate_all_scores(self):
         fold_score_list = [f for folds in self.list_of_folds for f in folds]
         all_scores = [s for fold in fold_score_list for s in fold.scores]
@@ -113,25 +116,36 @@ def get_all_folds_for_one_learner(learner,data,fold_idx):
         data: list of TimeSepForce objects to use
         fold_idx: list of <train,test>; one tuple per fold
     Returns:
-        list, one element per paramter. each element is a list of folds
+        tuple of :
+         (0) list, one element per paramter. each element is a list of folds
+         (1) list, one element per parameter. each element is a list of 
+         validation folds
     """
     func_to_call = learner.func_to_call
-    params_then_folds = []
+    params_then_folds,param_validation_fold = [],[]
     for param in learner.list_of_params:
         folds = []
+        folds_valid = []
         for train_idx,test_idx in fold_idx:
+            # get the training scores
             fold_data = [data[f] for f in train_idx]
             folds_tmp = single_fold_score(fold_data,func_to_call,param)
             folds.append(folds_tmp)
+            # get the validation scores
+            valid_data = [data[f] for f in test_idx]
+            valid_fold = single_fold_score(valid_data,func_to_call,param)
+            folds_valid.append(valid_fold)
         params_then_folds.append(folds)
-    return params_then_folds
+        param_validation_fold.append(folds_valid)
+    return params_then_folds,param_validation_fold
 
 def get_learners(n_points_no_event=7):
     """
     Returns a list of learning_curve objects
 
     Args:
-        None
+        n_points_no_event: number of points for varying the no event portion of 
+        things
     Returns:
         list of lerning curvess
     """
@@ -143,7 +157,7 @@ def get_learners(n_points_no_event=7):
                                     no_event_args_to_dict(no_event_tuple[1]))
     return [no_event_curve]
 
-def get_cached_folds(categories,force,cache_directory,limit,n_folds=2):
+def get_cached_folds(categories,force,cache_directory,limit,n_folds,seed=42):
     """
     caches all the results for every learner after reading in all the data
 
@@ -153,6 +167,7 @@ def get_cached_folds(categories,force,cache_directory,limit,n_folds=2):
         cache_directoy: where to put the pkl files
         limit: how many examples to read in 
         n_folds: now many folds to use
+        seed: for PRNG
     Returns:
         list, one element per paramter. each element is a list of folds
     """
@@ -163,10 +178,13 @@ def get_cached_folds(categories,force,cache_directory,limit,n_folds=2):
     labels = [l[0] for l in labels_data]
     data = [l[1] for l in labels_data]
     # determine the folds to use
-    fold_idx = StratifiedKFold(labels,n_folds)
+    fold_idx = StratifiedKFold(labels,n_folds=n_folds,shuffle=True,
+                               random_state=seed)
     learners = get_learners()
     # POST: all data read in. get all the scores for all the learners.
     for l in learners:
-        list_of_folds = get_all_folds_for_one_learner(l,data,fold_idx)
+        list_of_folds,validation_folds = \
+            get_all_folds_for_one_learner(l,data,fold_idx)
         l.set_list_of_folds(list_of_folds)
+        l.set_validation_folds(validation_folds)
     return learners
