9d9c7c0ef004fb866f4cb09fd7b6716af8ad1eba
Your Name <patirck.heenan@colorado.edu>
2017-02-27 21:58:45 -0700

    added in comments for timing stuff,

--- Personal/EventDetection/Timing/Timer.py
+++ Personal/EventDetection/Timing/Timer.py
@@ -22,10 +22,20 @@ class time_trials_by_loading_rate:
 
 
 def get_all_times(learner,data,list_of_curve_numbers,trials_per_curve_set=5):
+    """
+    gets the time_trials for a single loading rate (or whatever)
+
+    Args:
+         see cache_all_learners, except a single category's data
+    Returns:
+        a single time_trials object
+    """
     times_all_trials = []
     sizes_all_trials = []
     for l in list_of_curve_numbers:
         shuffled_data = random.shuffle(data)
+        if (l < len(shuffled_data)):
+            continue
         data_tmp = shuffled_data[:l]
         times = []
         sizes = [d.Force.size for d in data_tmp]
@@ -37,6 +47,14 @@ def get_all_times(learner,data,list_of_curve_numbers,trials_per_curve_set=5):
     return time_trial(times_all_trials,num_curves_all_trials,sizes_all_trials)
 
 def single_learner(learner,curve_numbers,categories,**kwargs):
+    """
+    gets the time_trials_by_loading_rate for a single object
+
+    Args:
+         see cache_all_learners, except a single learner
+    Returns:
+        a single time_trials_by_loading_rate
+    """
     for c in categories:
         data = c.data
         trials.append(get_all_times(learner,data,curve_numbers,**kwargs))
@@ -44,6 +62,18 @@ def single_learner(learner,curve_numbers,categories,**kwargs):
     return time_trials_by_loading_rate(learner,trials,loading_rates)
 
 def cache_all_learners(learners,categories,curves_numbers,**kwargs):
+    """
+    caches and returns the timing results for all the learners
+
+    Args:
+        learners: list ofLearning.learnin_curve object to use
+        categories: list of Learninrg.ForceExtensionCtegories to use
+        curve_numbers: how many curves to use (from each category;
+        they are all used separately)
+        **kwargs: passed to curve_numbers
+    Returns:
+        list of time_trials_by_loading_rate objects
+    """
     times = []
     for l in learners:
         t = CheckpointUtilities.getCheckpoint(l.description,
@@ -66,7 +96,10 @@ def run():
     learners = Learning.get_learners()
     positive_categories = Learning.get_categories(positives_directory)
     curve_numbers = [1,2,5,10,20,50,100,200]
-    times = cache_all_learners(learners,categories,curves_numbers)
+    for c in positive_categories:
+        Learning.category_read(c,force=False,cache_directory="./cache/",
+                               limit=max(curve_numbers))
+    times = cache_all_learners(learners,positive_categories,curves_numbers)
     print(times[0].list_of_time_trials[0].times)
 
 if __name__ == "__main__":
