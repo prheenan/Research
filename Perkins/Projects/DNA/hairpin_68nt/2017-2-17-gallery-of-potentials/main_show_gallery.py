261d1f455a9427b3ebece2074eca68ccbc50cd6c
patrick heenan <patrick.heenan@colorado.edu>
2017-02-28 15:57:46 -0700

    debugging the fjc stuff...

--- Perkins/Projects/DNA/hairpin_68nt/2017-2-17-gallery-of-potentials/main_show_gallery.py
+++ Perkins/Projects/DNA/hairpin_68nt/2017-2-17-gallery-of-potentials/main_show_gallery.py
@@ -10,23 +10,63 @@ from Research.Perkins.AnalysisUtil.ForceExtensionAnalysis import \
 from Research.Personal.EventDetection.Util import Analysis
 from GeneralUtil.python import PlotUtilities
 
+from scipy.interpolate import interp1d
+from scipy.optimize import brute
 
-def fjc_extension(L0,F,Lp,kbT,K0):
+def coth(x):
+    return 1/np.tanh(x)
+
+def fjc_extension(F,L0,Lp,kbT,K0):
     """
 
     See: 
     Wang, M. D., Yin, H., Landick, R., Gelles, J. & Block, S. M. 
-    Stretching DNA with optical tweezers. Biophys J 72, 1335–1346 (1997)
+    Stretching DNA with optical tweezers. Biophys J 72, 1335-1346 (1997)
     """
-    return L0 * np.coth( (2*F*Lp)/(kbT) - kbT/(2*F*Lp)) * (1+F/K0)
-
+    return L0 * (coth( (2*F*Lp)/(kbT)) - kbT/(2*F*Lp)) * (1+F/K0)
+    
+def fjc_model_force(F,*args,**kwargs):
+    force_range = [min(F),max(F)]
+    return np.linspace(*force_range,num=F.size)
+    
+def fjc_model_ext_and_force(F,*args,**kwargs):
+    force_modelled = fjc_model_force(F,*args,**kwargs)
+    ext_modelled = fjc_extension(force_modelled,*args,**kwargs)
+    return ext_modelled,force_modelled
+    
+def fjc_predicted_force_at_ext(separation,force,*args,**kwargs):
+    ext_modelled,force_modelled = fjc_model_ext_and_force(force,*args,**kwargs)
+    # interpolate back onto the grid we care about 
+    interp = interp1d(ext_modelled,force_modelled,bounds_error=False,
+                      fill_value="extrapolate")
+    predicted_force = interp(separation)
+    return predicted_force
+    
 def objective_l2(func_predict,true_values,*args):
     predicted_values = func_predict(*args)
-    return np.abs(predicted_values-true_values)**2
+    values = np.abs(predicted_values-true_values)**2
+    to_ret =  sum(values)/sum(true_values**2)
+    return to_ret
 
-def brute_optimize(func_to_call,brute_dict=None,*args,**kwargs):
-
-
+def brute_optimize(func_to_call,true_values,brute_dict=dict()):
+    objective = lambda *args: objective_l2(func_to_call,true_values,*args)
+    return brute(objective,disp=False,**brute_dict)
+    
+def fit_fjc_contour(separation,force,brute_dict=dict(),**kwargs):
+    func = lambda *args: \
+        fjc_predicted_force_at_ext(separation,force,*args,**kwargs)
+    ret = brute_optimize(func,force,brute_dict=brute_dict)
+    full_output_str = "full_output"
+    if ( (full_output_str in brute_dict) and (brute_dict[full_output_str])):
+        x0,fval,grid,Jout = ret
+    else:
+        x0 = ret
+    model_x, model_y = fjc_model_ext_and_force(force,*x0,**kwargs)
+    min_sep = min(separation)
+    idx = np.where(model_x >= min_sep)
+    return ret,model_x[idx],model_y[idx]
+    
+    
 def run():
     """
     <Description>
@@ -44,7 +84,7 @@ def run():
     out_dir = "./out/"
     force = False
     files,force_extension_curves = \
-        FEC_Util.read_and_cache_pxp(directory,force=false)
+        FEC_Util.read_and_cache_pxp(directory,force=force)
     force_extension_curves = [f for f in force_extension_curves]
     # split everything 
     curves_split= \
@@ -58,6 +98,24 @@ def run():
     # get just the retracts
     curves_retract = [ fec.retract for fec in curves_split]
     curves_approach = [ fec.approach for fec in curves_split]
+    # XXX debugging...
+    ex = curves_retract[2]
+    surface_idx = 1300
+    max_idx = np.argmax(ex.Force)
+    zeroed_force = ex.Force[surface_idx:max_idx]
+    zeroed_separation = ex.Separation[surface_idx:max_idx]
+    # ranges are for L0...
+    max_sep = max(zeroed_separation)
+    range_L0 = slice(0,max_sep,max_sep/50)
+    brute_dict = dict(ranges=[range_L0])
+    fjc_kwargs = dict(Lp=2.22e-9,kbT=4.1e-21,K0=1200e-12)
+    ret,model_x,model_y = fit_fjc_contour(zeroed_separation,zeroed_force,
+                                          brute_dict=brute_dict,**fjc_kwargs)
+    print(ret)
+    plt.plot(zeroed_separation,zeroed_force)
+    plt.plot(model_x,model_y)
+    plt.show()
+    exit(1)
     # plot as a 2-d histogram
     # filter to 1% of the size of the signal (should be loading-rate proof)
     histogram_filter_func = lambda o: int(np.ceil(o.Force.size * 0.01))
