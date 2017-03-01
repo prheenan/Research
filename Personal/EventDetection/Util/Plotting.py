39ad4cca9fbbf672a2243a64139d9c7dac5f9bbe
Your Name <patirck.heenan@colorado.edu>
2017-03-01 13:47:35 -0700

    mostly a cosmetic changes, added a function to plot the distance metric

--- Personal/EventDetection/Util/Plotting.py
+++ Personal/EventDetection/Util/Plotting.py
@@ -325,6 +325,43 @@ def plot_ruptures_of_scores(scores):
     rupture_predicted = [r for s in scores for r in s.ruptures_predicted ]
     plot_predicted_and_true_ruptures(rupture_true,rupture_predicted)
 
+def cross_validation_distance_metric(params,train_scores,valid_scores,to_true):
+    """
+    Plots the cross validation training and validation distance metric
+
+    Args:
+        params: x values
+        <train/valid>_scores: the training and validation scores used
+        to_true: distance metric plotted is *from* predicted *to* true 
+        (ie: something like recall) if true, otherwise vice versa
+
+    Returns:
+        nothing
+    """
+    # get the metrics and errors by parameters
+    score_func_pred = lambda x: Learning.median_dist_per_param(x,
+                                                               to_true=to_true)
+    error_func_pred = lambda x: Learning.stdev_dist_per_param(x,
+                                                              to_true=to_true)
+    x_values = np.array([p.values()[0] for p in params])
+    kw_pred = dict(score_func=score_func_pred,error_func=error_func_pred)
+    x_train,train_dist,train_dist_std = \
+        Learning.valid_scores_erors_and_params(x_values,train_scores,**kw_pred)
+    x_valid,valid_dist,valid_dist_std = \
+        Learning.valid_scores_erors_and_params(x_values,valid_scores,**kw_pred)
+    y_plot = lambda y: y * 1e9
+    train_dist_plot,train_error_plot = y_plot(train_dist),y_plot(train_dist_std)
+    valid_dist_plot,valid_error_plot = y_plot(valid_dist),y_plot(valid_dist_std)
+    style_train = dict(color='r',marker='o',linestyle='--',label="Training") 
+    style_valid = dict(color='g',marker='v',linestyle='-',label="Validation")
+    plt.errorbar(x=x_train,y=train_dist_plot,yerr=train_error_plot,
+                 **style_train)
+    plt.errorbar(x=x_valid,y=valid_dist_plot,yerr=valid_error_plot,
+                 **style_valid)
+    plt.xscale('log')
+    PlotUtilities.lazyLabel("Tuning Parameter","Median event distance (nm)","",
+                            frameon=True)
+
 
 def plot_individual_learner(cache_directory,learner):
     """
@@ -340,28 +377,8 @@ def plot_individual_learner(cache_directory,learner):
     # get the scoring objects by paramter by fold
     train_scores = learner._scores_by_params(train=True)
     valid_scores = learner._scores_by_params(train=False)
-    # get the metrics and errors by parameters
-    to_true = True
-    score_func_pred = lambda x: Learning.median_dist_per_param(x,
-                                                               to_true=to_true)
-    error_func_pred = lambda x: Learning.stdev_dist_per_param(x,
-                                                              to_true=to_true)
-    x_values = np.array([p.values()[0] for p in params])
-    kw_pred = dict(score_func=score_func_pred,error_func=error_func_pred)
-    x_train,train_dist,train_dist_std = \
-        Learning.valid_scores_erors_and_params(x_values,train_scores,**kw_pred)
-    x_valid,valid_dist,valid_dist_std = \
-        Learning.valid_scores_erors_and_params(x_values,valid_scores,**kw_pred)
-    y_plot = lambda y: y * 1e9
-    train_dist_plot,train_error_plot = y_plot(train_dist),y_plot(train_dist_std)
-    valid_dist_plot,valid_error_plot = y_plot(valid_dist),y_plot(valid_dist_std)
     fig = PlotUtilities.figure()
-    plt.errorbar(x=x_train,y=train_dist_plot,yerr=train_error_plot,
-                 color='r',marker='o',linestyle='--',label="Training")
-    plt.errorbar(x=x_valid,y=valid_dist_plot,yerr=valid_error_plot,
-                 color='g',marker='v',linestyle='-',label="Validation")
-    plt.xscale('log')
-    PlotUtilities.lazyLabel("Tuning Parameter","Median event distance (nm)","",
-                            frameon=True)
+    cross_validation_distance_metric(params,train_scores,valid_scores,
+                                     to_true=True)
     out_file_stem = cache_directory + "{:s}".format(learner.description)
     PlotUtilities.savefig(fig,out_file_stem + "dist.png")
