bd872d3d929e0d0b2c3a76d3602296be81c0dd70
Your Name <patirck.heenan@colorado.edu>
2017-03-04 16:43:54 -0700

    minor change affecting cheby

--- Personal/EventDetection/_2SplineEventDetector/Detector.py
+++ Personal/EventDetection/_2SplineEventDetector/Detector.py
@@ -321,12 +321,11 @@ def _event_probabilities(x,y,interp,n_points,threshold):
     denom = (stdev_masked-median_local_stdev)
     k_chebyshev = denom/scale
     # determine where the chebyshev is 'safe', otherwise we are at or above
-    # the median and hence not a useful metric
+    # the mean estimate and hence not a useful metric
     cheby_idx = np.where(k_chebyshev >= 1)
     chebyshev = np.ones(k_chebyshev.size)
-    k_chebyshev_safe = k_chebyshev[cheby_idx]
     # actually calculate the upper bound for the probability
-    chebyshev[cheby_idx] = (1/k_chebyshev_safe)**2
+    chebyshev[cheby_idx] = (1/k_chebyshev[cheby_idx])**2
     # for the edge cases, assume the probability is one                         
     probability_distribution = np.ones(y.size)          
     # get the probability for all the non edge cases
