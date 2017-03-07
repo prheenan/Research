0b5cc4b59ac3ea52b0b9a369fbdbd78c8f569ea1
patrick heenan <patrick.heenan@colorado.edu>
2017-03-07 16:57:17 -0700

    huge limit

--- Personal/EventDetection/_1ReadDataToCache/main_read_data.py
+++ Personal/EventDetection/_1ReadDataToCache/main_read_data.py
@@ -26,7 +26,7 @@ def run():
     """
     cache_directory = "./cache/"
     # limit (per category)
-    limit = 60
+    limit = 200
     n_folds = 5
     pool_size =  multiprocessing.cpu_count()-1
     force_read = False
