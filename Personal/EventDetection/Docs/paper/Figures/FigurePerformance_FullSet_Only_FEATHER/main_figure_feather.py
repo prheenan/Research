b0b20ca8451b6967ccd8b79c5f7a5a84169507df
Your Name <patirck.heenan@colorado.edu>
2017-03-19 16:21:37 -0600

    moved most of the definitions up

--- Personal/EventDetection/Docs/paper/Figures/FigurePerformance_FullSet_Only_FEATHER/main_figure_feather.py
+++ Personal/EventDetection/Docs/paper/Figures/FigurePerformance_FullSet_Only_FEATHER/main_figure_feather.py
@@ -30,9 +30,10 @@ def run(base="./"):
                               fig=fig)
         final_out_path = "{:s}_{:d}.pdf".format(out_base,i)
         PlotUtilities.label_tom(fig,loc=(-0.25,1.1),fontsize=18)
+        plt.suptitle("FEATHER",fontsize=25,y=0.95,fontcolor='b')
         PlotUtilities.savefig(fig,final_out_path,
                               subplots_adjust=dict(wspace=0.4,left=0.10,
-                                                   top=0.9))
+                                                   top=0.8))
 
 
 
