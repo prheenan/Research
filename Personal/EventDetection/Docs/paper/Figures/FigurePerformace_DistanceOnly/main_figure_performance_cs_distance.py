ca10bdb87aed8098288acd81a58d46a8a55122a0
Your Name <patirck.heenan@colorado.edu>
2017-03-27 12:26:14 -0600

    fixed the distance and rupture figures

--- Personal/EventDetection/Docs/paper/Figures/FigurePerformace_DistanceOnly/main_figure_performance_cs_distance.py
+++ Personal/EventDetection/Docs/paper/Figures/FigurePerformace_DistanceOnly/main_figure_performance_cs_distance.py
@@ -37,15 +37,16 @@ def make_distance_figure(l,data_file,fec_file):
     offset = min(plt.xlim())
     plt.xlim([-15,max_sep_nanometers*fudge])
     plt.ylim(ylim)
-    pred_idx = int(min(event_starts)/2)
+    pred_idx = int(min(event_starts)/4)
     # plot arrows from actual to predicted
     ax = plt.gca()
     head_length = 25
-    fudge_arrow_pN = 5
+    fudge_arrow_pN = 6
     _arrowprops = dict(shrinkA=0,shrinkB=0.1,
                        width=4.0,alpha=0.3,edgecolor='k')
     arrowprops_true = dict(facecolor='g',hatch='//',**_arrowprops)
     arrowprops_pred = dict(facecolor='b',**_arrowprops)
+    text_bbox_props = dict(alpha=0.0,fc='w',lw=0)
     for start in event_starts:
         dy = 0
         dx = plot_x[pred_idx]-plot_x[start]
@@ -55,7 +56,7 @@ def make_distance_figure(l,data_file,fec_file):
     text_loc_x = np.mean([plot_x[min(event_starts)],plot_x[pred_idx]])
     text_loc_y = xy[1] + fudge_arrow_pN
     ax.text(text_loc_x,text_loc_y,s=r"d$_{\mathrm{t}\rightarrow\mathrm{p}}$",
-            fontsize=20,color=arrowprops_true['facecolor'])
+            fontsize=20,color=arrowprops_true['facecolor'],bbox=text_bbox_props)
     # plot arrows from predicted to true
     y_pred = (max([(plot_y[pred_idx] + fudge_pN) for start in event_starts]) + \
               fudge_arrow_pN)
@@ -67,7 +68,7 @@ def make_distance_figure(l,data_file,fec_file):
     ax.annotate("", xy=xy_offset, xytext=xy_pred,arrowprops=arrowprops_pred)
     ax.text(x_pred*1.05,y_pred-fudge_arrow_pN*0.8,
             s=r"d$_{\mathrm{p}\rightarrow\mathrm{t}}$",
-            fontsize=20,color=arrowprops_pred['facecolor'])
+            fontsize=20,color=arrowprops_pred['facecolor'],bbox=text_bbox_props)
     plt.axvline(plot_x[pred_idx],linestyle='--',color='b',
                 label="Predicted event")
     # plot the events as arrows
@@ -90,15 +91,20 @@ def run(base="./"):
     cache_file = base + "cache.pkl"
     fec_file = data_base + "multiple.csv.pkl"
     name = "FEATHER"
+    final_out_hist = "{:s}{:s}_distances.pdf".format(out_base,
+                                                     name.replace(" ","_"))
     l = CheckpointUtilities.getCheckpoint(cache_file,get_feather_run,force,
                                           data_file)
+    # make the distance histogram figure for the presenation
+    fig = PlotUtilities.figure((10,5))
+    make_distance_figure(l,data_file,fec_file)
+    PlotUtilities.savefig(fig,final_out_hist.replace(".pdf","_pres.pdf"))
     # make the distance histogram figure
     fig = PlotUtilities.figure((16,6))
     make_distance_figure(l,data_file,fec_file)
-    final_out_hist = "{:s}{:s}_distances.pdf".format(out_base,
-                                                     name.replace(" ","_"))
     PlotUtilities.label_tom(fig,loc=(-0.1,1.0),fontsize=18)
     PlotUtilities.savefig(fig,final_out_hist)
+
 
 
 
--- Personal/EventDetection/Docs/paper/Figures/FigurePerformance_FullSet_Only_FEATHER/main_figure_feather.py
+++ Personal/EventDetection/Docs/paper/Figures/FigurePerformance_FullSet_Only_FEATHER/main_figure_feather.py
@@ -39,12 +39,12 @@ def run(in_base="../FigurePerformance_CS/"):
         distance_histogram= Offline.event_error_kwargs(m,color_pred=color_pred)
         true,pred = m.true,m.pred
         # make the 'just the distance' figures
-        fig = PlotUtilities.figure((16,6))
+        fig = PlotUtilities.figure((10,6))
         Plotting.histogram_event_distribution(**distance_histogram)
         final_out_dist = "{:s}{:s}_dist.pdf".format(out_base,safe_name)
         PlotUtilities.savefig(fig,final_out_dist)
         # make the rupture spectrum figure
-        fig = PlotUtilities.figure((16,8))
+        fig = PlotUtilities.figure((12,7))
         final_out_rupture = "{:s}{:s}_rupture.pdf".format(out_base,safe_name)
         Plotting.rupture_plot(true,pred,fig=fig)
         PlotUtilities.savefig(fig,final_out_rupture)
