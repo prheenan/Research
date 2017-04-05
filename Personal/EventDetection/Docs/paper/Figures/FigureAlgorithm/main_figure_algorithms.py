fe6bd1ce3d087c060b068726450049b9a5e14d25
Your Name <patirck.heenan@colorado.edu>
2017-04-04 19:53:25 -0600

    added transformations to the slides

--- Personal/EventDetection/Docs/paper/Figures/FigureAlgorithm/main_figure_algorithms.py
+++ Personal/EventDetection/Docs/paper/Figures/FigureAlgorithm/main_figure_algorithms.py
@@ -119,12 +119,11 @@ def plot_probability(threshold,x_plot,prob_final,slice_before,slice_after):
 
 
 
-def plot_epsilon(epsilon_plot,sigma_plot):
+def plot_epsilon(epsilon_plot,sigma_plot,label="$\epsilon \pm \sigma$"):
     epsilon_style = dict(color='k')
     min_v = epsilon_plot-sigma_plot
     max_v = epsilon_plot+sigma_plot
-    plt.axhspan(min_v,max_v,color='c',alpha=0.3,
-                label="$\epsilon \pm \sigma$")
+    plt.axhspan(min_v,max_v,color='c',alpha=0.3,label=label)
 
 def retract_figure(x_plot,force_plot,slice_before,slice_after,
                    force_filtered_plot,ylim_force,force_label,
@@ -153,6 +152,70 @@ def retract_figure(x_plot,force_plot,slice_before,slice_after,
     plot_probability(threshold,x_plot,prob,slice_before,slice_after)
     PlotUtilities.no_x_label()
     PlotUtilities.xlabel("")
+
+def spline_figures(fec_split,out_base,threshold=1e-2):
+    retract = fec_split.retract
+    x = retract.Time
+    f = retract.Force
+    interp = fec_split.retract_spline_interpolator()
+    interp_f = interp(x)
+    n_points = fec_split.tau_num_points
+    delta = _no_event._delta(x,interp_f,n_points)
+    approach_kwargs = Detector.make_event_parameters_from_split_fec(fec_split)
+    param_obj = _no_event.no_event_parameters(threshold=threshold,
+                                              **approach_kwargs)
+    # get the delta probability for negative and positive stuff. 
+    delta_prob_all = _no_event._delta_probability(delta,param_obj,
+                                                  negative_only=False)
+    delta_prob_neg = _no_event._delta_probability(delta,param_obj,
+                                                  negative_only=True)
+    y_plot_f = lambda x : x * 1e12
+    delta_plot = y_plot_f(delta)
+    delta_label = "dF (pN)"
+    delta_params = [y_plot_f(param_obj.epsilon),y_plot_f(param_obj.sigma)]
+    # get the negtive df parametrs; this is what we actually use
+    # (ie: df should be *below* the epsilon and sigm parameters)
+    delta_params_n = list(-1 * np.array(delta_params))
+    delta_p_label = r"$(\epsilon \pm \sigma)$"
+    delta_n_label = r"$-(\epsilon \pm \sigma)$"
+    # get the derivative stuff 
+    deriv_params = [y_plot_f(param_obj.derivative_epsilon),
+                    y_plot_f(param_obj.derivative_sigma)]
+    derivative = _no_event._spline_derivative(x,interp)
+    deriv_plot = y_plot_f(derivative)
+    deriv_prob = _no_event._derivative_probability(interp,x,
+                                                   param_obj,
+                                                   negative_only=True)
+    deriv_label = r"$\frac{dF}{dt}$ (pN/s)"
+    deriv_p_label = r"$\epsilon_{\frac{dF}{dt}} \pm \sigma_{\frac{dF}{dt}}$"
+    # get the integral stuff 
+    integral = _no_event.local_noise_integral(f,interp_f,n_points,param_obj)
+    integ_plot = y_plot_f(integral)
+    integ_prob = _no_event._integral_probability(f,interp_f,n_points,param_obj)
+    integ_params = [y_plot_f(param_obj.integral_epsilon),
+                    y_plot_f(param_obj.integral_sigma)]
+    integ_label = r"$\int r_t dt$ (pN $\cdot$ s)"
+    integ_p_label = r"$\epsilon_{\int} \pm \sigma_{\int}$"
+    # plot all the values and their distributions
+    plot_tuples = [ \
+        ([delta_plot,delta_prob_all,delta_label,delta_p_label] +delta_params),
+        ([delta_plot,delta_prob_neg,delta_label,delta_n_label] +delta_params_n),
+        ([deriv_plot,deriv_prob    ,deriv_label,deriv_p_label] +deriv_params),
+        ([integ_plot,integ_prob    ,integ_label,integ_p_label] +integ_params),
+                    ]
+    style_plotting = dict(color='k')
+    for i,(val,prob,y_label,eps_label,eps,sigma) in enumerate(plot_tuples):
+        fig = PlotUtilities.figure((8,6))
+        plt.subplot(2,1,1)
+        plt.plot(x,val,**style_plotting)
+        plot_epsilon(eps,sigma,label=eps_label)
+        PlotUtilities.lazyLabel("Time (s)",y_label,"")
+        PlotUtilities.x_label_on_top()
+        plt.subplot(2,1,2)
+        plt.semilogy(x,prob,**style_plotting)
+        PlotUtilities.lazyLabel("","Probability","")
+        PlotUtilities.no_x_label()
+        PlotUtilities.savefig(fig,out_base + "spline{:d}.pdf".format(i))
 
 def run(base="./"):
     """
@@ -234,6 +297,10 @@ def run(base="./"):
     n_rows = 3
     n_cols = 2
     subplots_adjust_pres = dict(hspace=0.1)
+    """
+    Make the spline probability figures
+    """
+    spline_figures(fec_split,out_base="./explaining_")
     """
     make just the retract figures
     """
