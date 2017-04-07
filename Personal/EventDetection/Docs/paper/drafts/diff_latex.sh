097511fa0bc75155d9b15afeba065d5ddbfd6650
Your Name <patirck.heenan@colorado.edu>
2017-04-07 15:08:53 -0600

    actually doing the whole git diff thing. make the command call much cleaner with comments on why it is the way it is.

--- Personal/EventDetection/Docs/paper/drafts/diff_latex.sh
+++ Personal/EventDetection/Docs/paper/drafts/diff_latex.sh
@@ -16,8 +16,16 @@ dateStr=`date +%Y-%m-%d:%H:%M:%S`
 #### Arg 1: Description
 #rev=d61409367a84cd7389ecc1cd97d37bd0369520d1
 rev=0f0d13f088c4bba06a3566f4a5efbcda3b6ef161
-git-latexdiff --ignore-latex-errors -b --main prheenan_thesis_ms.tex $rev -- 
-
-
-
+#  need to use --append-context2cmd to tract abstract, see
+# researchgate.net/post/How_can_I_track_changes_in_LaTeX_especially_abstractenvironment
+context_append_str="abstract,appendix,bibliography"
+# essentially, ignore lstlisting:
+# tex.stackexchange.com/questions/136786/latexdiff-and-lstlistinga
+config_str='PICTUREENV=(?:picture|DIFnomarkup|lstlisting)[\w\d*@]*'
+git-latexdiff -b \
+	      --ignore-latex-errors \
+	      --main prheenan_thesis_ms.tex $rev \
+	      -- \
+	      --config=$config_str \
+	      --append-context2cmd=$config_str
 
