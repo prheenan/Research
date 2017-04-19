420faed489d0d63831b21ea662d3bbb70e7aa2c3
patrick heenan <patrick.heenan@colorado.edu>
2017-04-19 13:57:18 -0600

    switched to checkpoint fitting of the FJC model.

--- Perkins/Projects/DNA/hairpin_100nt/2017-4-19-initial-test/main_hairpin_100nt_initial.py
+++ Perkins/Projects/DNA/hairpin_100nt/2017-4-19-initial-test/main_hairpin_100nt_initial.py
@@ -22,7 +22,22 @@ def get_single_example(abs_dir):
                                                            force=False)
     example = force_extension_curves[0]
     return example
-
+    
+def get_wlc_information(sep,force,sep_bounds,**kwargs):
+    models = []                   
+    for min_sep,max_sep in sep_bounds:
+        where_fit = np.where( (sep <= max_sep) & (sep >= min_sep))
+        sep_fit = sep[where_fit]
+        brute_dict = dict(ranges=[[max_sep/2,max_sep*2]],**kwargs)
+        x0,model_x,model_y = FJC.fit_fjc_contour(sep_fit,
+                                                 force[where_fit],
+                                                 Lp=0.75e-9,
+                                                 kbT=4.1e-21,
+                                                 K0=800e-12,
+                                                 brute_dict=brute_dict)
+        models.append([x0,model_x,model_y]) 
+    return models        
+        
 def run():
     """
     <Description>
@@ -38,24 +53,21 @@ def run():
               "4Patrick/CuratedData/DNA/hairpin-100nt-16gc/Positive"
     example = CheckpointUtilities.getCheckpoint("./single.pkl",get_single_example,
                                                 False,abs_dir)
+    i=0                                              
     example_split = Analysis.zero_and_split_force_extension_curve(example)
+    example_split,pred_info = Detector._predict_full(example,threshold=1e-4)
+    retract = example_split.retract
     sep = example_split.retract.Separation
     sep -= min(sep)
+    last = pred_info.event_idx[-1]
+    last_x = sep[last]
+    delta_x = 40e-9    
     force = example_split.retract.Force
-    sep_bounds = [ [45e-9,71e-9],
-                   [10e-9,40e-9]]
-    models = []                   
-    for min_sep,max_sep in sep_bounds:
-        where_fit = np.where( (sep <= max_sep) & (sep >= min_sep))
-        sep_fit = sep[where_fit]
-        brute_dict = dict(ranges=[[max_sep/2,max_sep*1.5]],Ns=10)
-        x0,model_x,model_y = FJC.fit_fjc_contour(sep_fit,
-                                                 force[where_fit],
-                                                 Lp=0.75e-9,
-                                                 kbT=4.1e-21,
-                                                 K0=800e-12,
-                                                 brute_dict=brute_dict)
-        models.append([x0,model_x,model_y]) 
+    sep_bounds = [ [delta_x,last_x],
+                   [0,delta_x]]
+    models = CheckpointUtilities.getCheckpoint("./model{:d}.pkl".format(i),
+                                               get_wlc_information,
+                                               False,sep,force,sep_bounds,Ns=10)
     plt.plot(sep,force)
     for x0,model_x,model_y in models:
         print(x0)
