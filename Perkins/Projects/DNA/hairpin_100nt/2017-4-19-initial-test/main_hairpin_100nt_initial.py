e46ad49456fe0034ed24d8d1193fc471fce2367d
patrick heenan <patrick.heenan@colorado.edu>
2017-04-19 16:20:11 -0600

    working on DNA hairpin reader...

--- Perkins/Projects/DNA/hairpin_100nt/2017-4-19-initial-test/main_hairpin_100nt_initial.py
+++ Perkins/Projects/DNA/hairpin_100nt/2017-4-19-initial-test/main_hairpin_100nt_initial.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 # This file is used for importing the common utilities classes.
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+import sys,cProfile
 
 sys.path.append("../../../../../../")
 from Research.Perkins.AnalysisUtil.ForceExtensionAnalysis import \
@@ -17,30 +17,42 @@ from Research.Personal.EventDetection._2SplineEventDetector import Detector
 from GeneralUtil.python import PlotUtilities,CheckpointUtilities
 from FitUtil.FreelyJointedChain.Python.Code import FJC
 
-def get_single_example(abs_dir):
-    _,force_extension_curves = FEC_Util.read_and_cache_pxp(abs_dir,
-                                                           force=False)
-    example = force_extension_curves[0]
-    return example
     
 def get_wlc_information(sep,force,sep_bounds,**kwargs):
-    models = []                   
+    models = []         
+    
     for min_sep,max_sep in sep_bounds:
         where_fit = np.where( (sep <= max_sep) & (sep >= min_sep))
         sep_fit = sep[where_fit]
-        brute_dict = dict(ranges=[[max_sep/2,max_sep*2]],**kwargs)
-        pr = cProfile.Profile()
-        pr.enable()
+        min_range = np.mean([min_sep,max_sep])
+        brute_dict = dict(ranges=[[0.9*max_sep,1.1*max_sep]],**kwargs)
         x0,model_x,model_y = FJC.fit_fjc_contour(sep_fit,
                                                  force[where_fit],
                                                  Lp=0.75e-9,
                                                  kbT=4.1e-21,
                                                  K0=800e-12,
                                                  brute_dict=brute_dict)
-        pr.disable()
-        pr.print_stats(sort='time')                         
+                    
         models.append([x0,model_x,model_y]) 
-    return models        
+    return models
+    
+def get_basic_information(i,example,force_run=False):
+    example_split = Analysis.zero_and_split_force_extension_curve(example)
+    example_split,pred_info = Detector._predict_full(example,threshold=1e-4)
+    retract = example_split.retract
+    sep = example_split.retract.Separation
+    sep -= min(sep)
+    last = pred_info.event_idx[-1]
+    last_x = sep[last]
+    delta_x = 40e-9    
+    force = example_split.retract.Force
+    sep_bounds = [ [delta_x*1.2,last_x],
+                   [0,delta_x]]
+    models = CheckpointUtilities.getCheckpoint("./model{:d}.pkl".format(i),
+                                               get_wlc_information,
+                                               force_run,sep,force,sep_bounds,
+                                               Ns=30)
+    return models,sep,force                                                 
         
 def run():
     """
@@ -55,28 +67,37 @@ def run():
     base_data_dir = FEC_Util.default_data_root()
     abs_dir = base_data_dir + \
               "4Patrick/CuratedData/DNA/hairpin-100nt-16gc/Positive"
-    example = CheckpointUtilities.getCheckpoint("./single.pkl",get_single_example,
-                                                False,abs_dir)
-    i=0                                              
-    example_split = Analysis.zero_and_split_force_extension_curve(example)
-    example_split,pred_info = Detector._predict_full(example,threshold=1e-4)
-    retract = example_split.retract
-    sep = example_split.retract.Separation
-    sep -= min(sep)
-    last = pred_info.event_idx[-1]
-    last_x = sep[last]
-    delta_x = 40e-9    
-    force = example_split.retract.Force
-    sep_bounds = [ [delta_x,last_x],
-                   [0,delta_x]]
-    models = CheckpointUtilities.getCheckpoint("./model{:d}.pkl".format(i),
-                                               get_wlc_information,
-                                               True,sep,force,sep_bounds,Ns=10)
-    plt.plot(sep,force)
-    for x0,model_x,model_y in models:
-        print(x0)
-        plt.plot(model_x,model_y)
-    plt.show()
+    _, examples = FEC_Util.read_and_cache_pxp(abs_dir,force=False)
+    args = []
+    force_run = True
+    for i,example in enumerate(examples):
+        # get the FJC model...
+        models,sep,force = CheckpointUtilities.getCheckpoint(
+            "./model_all{:d}.pkl".format(i),get_basic_information,
+            force_run,i,example)
+        args.append([models,sep,force])
+    models_all = [ [x0_x_y_tuple[0] for x0_x_y_tuple in list_v[0]] 
+                   for list_v in args]
+    first_l = np.concatenate([m[1] for m in models_all])
+    last_l = np.concatenate([m[0] for m in models_all])
+    print(first_l)
+    style_common = dict(alpha=0.3)
+    style_line = dict(linewidth=3,linestyle='--')
+    fig = PlotUtilities.figure()                                                       
+    plt.hist(first_l*1e9,label="first FJC",**style_common)
+    plt.hist(last_l*1e9,label="second FJC",**style_common)
+    plt.axvline(43,**style_line)
+    plt.axvline(67,**style_line)
+    PlotUtilities.lazyLabel("Contour Length (nm)","Count","")
+    PlotUtilities.savefig(fig,"./out/o_hist{:d}.png".format(i))   
+    for i,(models,sep,force) in enumerate(args):
+        fig = PlotUtilities.figure()                                                   
+        plt.plot(sep,force)
+        for x0,model_x,model_y in models:
+            plt.plot(model_x,model_y)
+        plt.xlim([min(sep),max(sep)])
+        PlotUtilities.lazyLabel("Separation (nm)","Force (pN)","")
+        PlotUtilities.savefig(fig,"./out/out{:d}.png".format(i))
     
 if __name__ == "__main__":
     run()
