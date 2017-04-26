5bc7179e6ed2bb1ab917ac4bd533ce5fbc413dbb
Your Name <patirck.heenan@colorado.edu>
2017-04-26 12:26:11 -0600

    using better picling routine to make the code easier to run

--- Perkins/Projects/Protein/titin/2017-1-17-solvent-cystein/main.py
+++ Perkins/Projects/Protein/titin/2017-1-17-solvent-cystein/main.py
@@ -1,21 +1,17 @@
+# (c) patrick raymond heenan, 2017, all right reserved
 import __main__
 __main__.pymol_argv = ['pymol','-qc']
 import pymol
 from pymol import cmd, stored
+import numpy as np,pickle
 
-import numpy as np
-# god forgive me for absolute paths
-sys.path.append("/Users/patrickheenan/src_prh/")
-from GeneralUtil.python import CheckpointUtilities
-
-
-def GetArea(identifier,solvent_area,dot_density=4,solvent_radius=1.4):
+def GetArea(identifier,solvent_area,dot_density=0,solvent_radius=1.4):
     """
     Args:
         identifier: the idenfier for what we are getting the area of;
         passed to cmd.get_area
 
-        dot_density: the density of dots, between 0 and 1
+        dot_density: the density of dots, between 0 and 4
         
         solvent_area,: if True, get the solvent-accessible area. Otherwise,
         just get the surface area
@@ -58,12 +54,17 @@ def run():
     (1) open pymol
     (2) in the command window, type:
     run /Users/patrickheenan/src_prh/Research/Perkins/Projects/Protein/titin/2017-1-17-solvent-cystein/main.py
-    (3) ???
-    (4) Profit
+    (replacing whereever the file is. Note you must also change 'base' below
+    to point to where thre file lives. 
+    (3) Then this will spit out a CSV file with accessibility information on 
+    cystines. it also colors the cystines red/green if they are/arent accessible
+    (in pymol)
     """
     # reset the session, make the background less awful
     cmd.delete("all")
     cmd.bg_color('grey80')
+    # need to replace this with wherever the pdb file is...
+    # must be an abolute path
     base="/Users/patrickheenan/src_prh/Research/Perkins/Projects/Protein/" +\
         "titin/2017-1-17-solvent-cystein/"
     """
@@ -79,11 +80,17 @@ def run():
     cmd.iterate(name_cystines, 'stored.residues.append(resi)')
     cmd.iterate(name_cystines, 'stored.names.append(name)')
     ids = [float(r) for r in stored.residues]
-    sasa_per_residue,area_per_residue= \
-        CheckpointUtilities.getCheckpoint(base + "out.pkl",
-                                          GetAllAreas,True,
-                                          stored.chains,stored.residues,
-                                          stored.names)
+    cache_file = base + "cache.pkl"
+    if (os.path.isfile(cache_file)):
+        with open(cache_file,'rb') as f:
+            sasa_per_residue,area_per_residue = pickle.load(f)
+    else:
+        sasa_per_residue,area_per_residue= GetAllAreas(stored.chains,
+                                                       stored.residues,
+                                                       stored.names)
+        with open(cache_file,'wb') as f:
+            pickle.dump((sasa_per_residue,area_per_residue),f,
+                        protocol=pickle.HIGHEST_PROTOCOL)
     accessible_tmp = np.where(sasa_per_residue > 
                               maximum_accessible_area_angstroms_squared)[0]
     ids_greater_than = [stored.residues[i] for i in accessible_tmp]
@@ -91,7 +98,7 @@ def run():
     cmd.show("spheres",name_cystines)
     cmd.color("green",name_cystines)
     accessible_name = "accessible_cys"
-    # color the accessible ones black
+    # color the accessible ones red
     cmd.select(accessible_name,"resi " + "+".join(ids_greater_than))
     cmd.color("red",accessible_name)
     thiol_name = "thiol"
