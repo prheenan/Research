19cb7af4bd02d7453a87f021ec3282c77e7cdedf
patrick heenan <patrick.heenan@colorado.edu>
2017-05-23 09:44:27 -0600

    switched landscape; minor changes following making the Szabo unit tests pass again

--- Perkins/Projects/Protein/bacteriorhodopsin/2017-3-6-inverse_weierstrass/main_iwt_initial_br.py
+++ Perkins/Projects/Protein/bacteriorhodopsin/2017-3-6-inverse_weierstrass/main_iwt_initial_br.py
@@ -34,21 +34,15 @@ def run():
     absolute_data_dir = base + "4Patrick/BRFEC/FEC_3ums/"
     downsample_n = 1
     fraction_for_vel = 0.1    
-    limit = 3
+    limit = 30
     force_sample = False
     force= False
     cache_directory = "./"
     out_base = cache_directory    
-    retracts = CheckpointUtilities.getCheckpoint("downsample.pkl",
-                                                 IoUtilHao.get_downsampled_data,
-                                                 force,
-                                                 downsample_n,force_sample,
-                                                 cache_directory,
-                                                 absolute_data_dir,limit=limit)
+    retracts = IoUtilHao.get_downsampled_data(downsample_n,force_sample,
+                                              cache_directory,
+                                              absolute_data_dir,limit=limit)
     data_iwt = [IoUtilHao.get_retract_pulling_region(d) for d in retracts]
-    fig = PlotUtilities.figure()
-    FEC_Plot.heat_map_fec(data_iwt)
-    PlotUtilities.savefig(fig,"heatmap.png")
     # POST: they are all set. get the IWT 
     num_bins = 250
     pr = cProfile.Profile()
@@ -57,6 +51,9 @@ def run():
         FreeEnergyAtZeroForce(data_iwt,NumBins=num_bins)
     pr.disable()
     pr.print_stats(sort='time')
+    fig = PlotUtilities.figure()
+    FEC_Plot.heat_map_fec(data_iwt)
+    PlotUtilities.savefig(fig,"heatmap.png")    
     force_N = [1e-12,5e-12,10e-12,20e-12,40e-12,100e-12,250e-12,500e-12]
     for i,f in enumerate(force_N):
         fig = PlotUtilities.figure(figsize=(12,12))
