cbeb317352534a17be3db8d3a4ff3d4d296330db
patrick heenan <patrick.heenan@colorado.edu>
2017-06-21 10:24:48 -0600

    removed approach switch in FEC_Plot.py. slightly better  commentin g

--- Perkins/Projects/DNA/hairpin_100nt/2017-6-14-high-resolution/main_hairpin_high_resolution.py
+++ Perkins/Projects/DNA/hairpin_100nt/2017-6-14-high-resolution/main_hairpin_high_resolution.py
@@ -100,6 +100,33 @@ def get_unfolding_slice_only(split_fec):
                                                         slice_unfolding_only)
     return unfolding_only
     
+def make_energy_landscape_plots(out_dir,energy_landscape_unfolding):
+    f_one_half_N_arr = np.array([1,2,5,7,8.5,10,12])*1e-12    
+    limits_kT = [-2,15]
+    # plot the iwt transform as just a free landscape                      
+    fig = PlotUtilities.figure()
+    IWT_Plot.plot_free_landscape(energy_landscape_unfolding)
+    PlotUtilities.xlabel("Separation (nm)")
+    PlotUtilities.savefig(fig,out_dir + "free_landscape.png")
+    n_ssDNA_nucleotides = 100
+    n_ssDNA_gc_rich = 32
+    rise_per_bp_ssDNA = 0.5
+    line_locations_nm = [\
+        7,
+        (n_ssDNA_nucleotides-n_ssDNA_gc_rich)*rise_per_bp_ssDNA,
+        n_ssDNA_nucleotides*rise_per_bp_ssDNA]
+    for f_one_half_N in f_one_half_N_arr:
+        fig = PlotUtilities.figure()    
+        ax_kcal = IWT_Plot.plot_single_landscape(energy_landscape_unfolding,
+                                                 f_one_half_N=f_one_half_N)
+        title = out_dir + \
+            "landscape_tilt_{:.1f}pN.png".format(f_one_half_N*1e12)
+        # reset the y limits 
+        plt.ylim(limits_kT)
+        IWT_Plot._set_kcal_axis_based_on_kT(plt.gca(),ax_kcal)            
+        for l in line_locations_nm:
+            plt.axvline(l)
+        PlotUtilities.savefig(fig,title)       
 def run():
     """
     <Description>
@@ -146,6 +173,7 @@ def run():
     for L0,split_fec in zip(contour_L0,good_splits):
         split_fec.retract.Separation -= L0
         split_fec.retract.Separation += arbitrary_offset
+    # POST: retracts are all aligned.         
     # for a simple IWT, only look at until the unfolding region
     unfolding_retracts = [get_unfolding_slice_only(split_fec) 
                           for e in good_splits]
@@ -161,33 +189,7 @@ def run():
         getCheckpoint("./landscape.pkl",
                       InverseWeierstrass.FreeEnergyAtZeroForce,True,
                       unfolding_iwt,NumBins=n_bins)
-    f_one_half_N_arr = np.array([1,2,5,7,8.5,10,12])*1e-12    
-    limits_kT = [-2,15]
-    # plot the iwt transform as just a free landscape                      
-    fig = PlotUtilities.figure()
-    IWT_Plot.plot_free_landscape(energy_landscape_unfolding)
-    PlotUtilities.xlabel("Separation (nm)")
-    PlotUtilities.savefig(fig,out_dir + "free_landscape.png")
-    n_ssDNA_nucleotides = 100
-    n_ssDNA_gc_rich = 32
-    rise_per_bp_ssDNA = 0.5
-    line_locations_nm = [\
-        7,
-        (n_ssDNA_nucleotides-n_ssDNA_gc_rich)*rise_per_bp_ssDNA,
-        n_ssDNA_nucleotides*rise_per_bp_ssDNA]
-    for f_one_half_N in f_one_half_N_arr:
-        fig = PlotUtilities.figure()    
-        ax_kcal = IWT_Plot.plot_single_landscape(energy_landscape_unfolding,
-                                                 f_one_half_N=f_one_half_N)
-        title = out_dir + \
-            "landscape_tilt_{:.1f}pN.png".format(f_one_half_N*1e12)
-        # reset the y limits 
-        plt.ylim(limits_kT)
-        IWT_Plot._set_kcal_axis_based_on_kT(plt.gca(),ax_kcal)            
-        for l in line_locations_nm:
-            plt.axvline(l)
-        PlotUtilities.savefig(fig,title)          
-    # POST: retracts are all aligned. 
+    make_energy_landscape_plots(out_dir,energy_landscape_unfolding)
     # make a heat map of all retracts 
     fig = PlotUtilities.figure()
     FEC_Plot.heat_map_fec([r.retract for r in good_splits])
