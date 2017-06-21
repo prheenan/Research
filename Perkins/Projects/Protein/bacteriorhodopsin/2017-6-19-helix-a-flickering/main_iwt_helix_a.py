60d838c0000de6deb83b9036364e9a68ef1c1bd8
patrick heenan <patrick.heenan@colorado.edu>
2017-06-21 16:30:13 -0600

    full data is now passed by an array

--- Perkins/Projects/Protein/bacteriorhodopsin/2017-6-19-helix-a-flickering/main_iwt_helix_a.py
+++ Perkins/Projects/Protein/bacteriorhodopsin/2017-6-19-helix-a-flickering/main_iwt_helix_a.py
@@ -47,24 +47,21 @@ def run():
     force_read_data = False
     force_iwt = True
     raw_data = IoUtilHao.read_and_cache_data_hao(in_dir,force=force_read_data,
-                                                 limit=10)
+                                                 limit=150)
     adhesion_end_m = 20e-9         
     offset_force_N = 7.1e-12
-    min_max_full = [adhesion_end_m,75e-9]
-    n_bins_zoom = 100
-    n_bins_helix_a = 150
+    n_bins_zoom = 75
+    n_bins_helix_a = 100
     array = [\
+        slice_area([adhesion_end_m,75e-9],"Full (no adhesion)",n_bins_helix_a),
         slice_area([20e-9,27e-9],"Helix A",n_bins_helix_a),
         slice_area([50e-9,75e-9],"Helix E",n_bins_zoom),
         slice_area([57e-9,70e-9],"Helix E (detailed)",n_bins_zoom)
         ]
     # # get the slice we care about for each...
     sliced_data = [ [] for _ in array]
-    full_data = []
     for i,d in enumerate(raw_data):
         d.Force -= offset_force_N 
-        sliced_fec = FEC_Util.slice_by_separation(d,*min_max_full)
-        full_data.append(sliced_fec)
         for i,a in enumerate(array):
             slice_tmp = FEC_Util.slice_by_separation(d,*a.ext_bounds)
             sliced_data[i].append(slice_tmp)
@@ -73,13 +70,6 @@ def run():
     n_bins_helix=200
     n_bins_helix_zoom = 60
     iwt_f = InverseWeierstrass.FreeEnergyAtZeroForce
-    # convert into the iwt objects needed 
-    iwt_full_data= IWT_Util.convert_to_iwt(full_data)
-    # get the proper landscapes
-    iwt_full = CheckpointUtilities.getCheckpoint(out_dir + "iwt_full.pkl",iwt_f,
-                                                 force_iwt,
-                                                 iwt_full_data,
-                                                 NumBins=n_bins)
     iwt_helices = []
     for i,a in enumerate(array):
         save_name = (out_dir + a.save_name)
@@ -89,28 +79,17 @@ def run():
                                                     iwt_helix_data_tmp,
                                                     NumBins=a.n_bins)  
         iwt_helices.append(iwt_tmp)                                                  
-    # make the iwt (energy landscape) plot of the entire protein
-    fig = PlotUtilities.figure()
-    obj_full_plot = IWT_Plot.plot_free_landscape(iwt_full)    
-    plt.axvspan(0,min(obj_full_plot.landscape_ext_nm),alpha=0.3,color='r',
-                label="Adhesion region",hatch='/')
-    PlotUtilities.title("Full bacteriorhodopsin energy landscape")
-    fmt_iwt()
-    PlotUtilities.legend()    
-    PlotUtilities.savefig(fig,out_dir + "iwt_full.png")
-    # plot each of the supregions 
+    # plot each of the subregions 
     for i,(a,data) in enumerate(zip(array,sliced_data)):
         fig = PlotUtilities.figure()
         IWT_Plot.plot_free_landscape(iwt_helices[i])    
         fmt_iwt()    
+        if (i ==0):
+            plt.axvspan(0,min(iwt_helices[i].Extensions * 1e9),alpha=0.3,
+                        color='r',label="Adhesion region",hatch='/')        
         PlotUtilities.title(a.plot_title)
         PlotUtilities.savefig(fig,out_dir + a.save_name + "_iwt_.png")
-    # # make the plots we want
-    # make a heat map of all the data and the 
-    fig = PlotUtilities.figure()
-    FEC_Plot.heat_map_fec(full_data)
-    PlotUtilities.savefig(fig,out_dir + "heat_map.png")
-    # plot the zoomed regions 
+    # # make the heat map plots we want
     for i,(a,data) in enumerate(zip(array,sliced_data)):
         fig = PlotUtilities.figure()
         FEC_Plot.heat_map_fec(sliced_data[i])    
@@ -119,7 +98,16 @@ def run():
     # plot each fec individually 
     to_x = lambda x : x*1e9
     to_y = lambda y : y*1e12
-    return 
+    # plot fec for each region(after the first, which is just the 'full_data')
+    for i,(a,d) in enumerate(zip(array,sliced_data)):
+        for fec in d:
+            fec_name = GenUtilities.file_name_from_path(fec.Meta.SourceFile)
+            save_name = out_dir + "regions_" + a.save_name + fec_name + ".png"
+            fig = PlotUtilities.figure()
+            FEC_Plot._fec_base_plot(to_x(fec.Separation),to_y(fec.Force))
+            plt.xlim(1e9 * np.array(a.ext_bounds))
+            PlotUtilities.savefig(fig,save_name)                                            
+    # plot each landscape entirely 
     for i,d in enumerate(raw_data):
         fig = PlotUtilities.figure()
         plt.plot(to_x(d.Separation),to_y(d.Force),color='k',alpha=0.3)
