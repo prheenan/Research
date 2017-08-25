0a79636e47de2e1cede316920e5be757fbe491da
Your Name <patirck.heenan@colorado.edu>
2017-08-25 12:44:07 -0600

    added in figure to refresh

---
+++ Personal/EventDetection/Docs/paper/drafts_biophys/drafts/scratch_md/refresh_figures.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# courtesy of: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# (helps with debugging)
+# set -e: immediately exit if we find a non zero
+# set -u: undefined references cause errors
+# set -o: single error causes full pipeline failure.
+set -euo pipefail
+IFS=$'\n\t'
+# datestring, used in many different places...
+dateStr=`date +%Y-%m-%d:%H:%M:%S`
+
+# Description:
+
+# Arguments:
+#### Arg 1: Description
+
+# Returns:
+
+# copy all .png or .svg files in the protein directory
+# (-o is OR clause)
+find ../../../Figures/BiophysicalJournal/Protein/ \
+    \( -name "*.png" -o -name "*.svg" \) -exec cp {} figures \;
