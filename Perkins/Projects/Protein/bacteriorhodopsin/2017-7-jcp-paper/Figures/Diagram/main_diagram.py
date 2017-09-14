ca3a283ec441eacd22be72cf9b89cc95e1550929
patrick heenan <patrick.heenan@colorado.edu>
2017-09-14 10:14:27 -0600

    fixed most of the figure 1 suff, except the arrows...

--- Perkins/Projects/Protein/bacteriorhodopsin/2017-7-jcp-paper/Figures/Diagram/main_diagram.py
+++ Perkins/Projects/Protein/bacteriorhodopsin/2017-7-jcp-paper/Figures/Diagram/main_diagram.py
@@ -56,14 +56,14 @@ def run():
     plot_examples = [example_plot]
     x_func = lambda y: y.Separation
     y_func = lambda y: y.Force 
-    ylim_pN = [-20,None]
-    xlim_nm = [-5,100]
+    ylim_pN = [-20,155]
+    xlim_nm = [5,75]
     zoom_regions_nm = [ [61.5,63]]
     adhesion_max_nm = 19
     # plot the helical regions...
     regions_nm = [ [[adhesion_max_nm,30],"ED Helix",'royalblue'],
                    [[30,45],"CB Helix",'orangered'],
-                   [[45,66],"A Helix",'g']]
+                   [[45,67],"A Helix",'g']]
     colors_regions = [regions_nm[-1]]                        
     # slice the regions 
     regions = [FEC_Util.slice_by_separation(example_plot,*reg) 
@@ -75,12 +75,9 @@ def run():
     top_spec = gridspec.GridSpec(2,3)
     # create separate axes for the image and FECs 
     image_spec = \
-        gridspec.GridSpecFromSubplotSpec(1,1,subplot_spec=top_spec[0,0],
-                                        hspace=0,
-                                        wspace=0)
+        gridspec.GridSpecFromSubplotSpec(1,1,subplot_spec=top_spec[0,0])
     data_spec = \
-        gridspec.GridSpecFromSubplotSpec(1,2,subplot_spec=top_spec[0,1:],
-                                         hspace=0.05,wspace=0.12)
+        gridspec.GridSpecFromSubplotSpec(1,2,subplot_spec=top_spec[0,1:])
     # # plot the image 
     ax = plt.subplot(image_spec[:])
     plt.imshow(plt.imread("../Data/sample_cartoon.png"),aspect='auto')
@@ -99,7 +96,6 @@ def run():
                             **dict_plot)
     PlotUtilities.tom_ticks(ax=ax_example,num_major=5,change_x=False)    
     PlotUtilities.tom_ticks(ax=ax_example,num_major=4,change_y=False)                              
-
     for i,(r,color) in enumerate(zip(regions,colors_regions)):
         dict_tmp = dict(**dict_plot)
         dict_tmp['style_data']['color'] = color[-1]
@@ -109,30 +105,59 @@ def run():
     for x,name,color in regions_nm:
         plt.axvspan(*x,color=color,alpha=0.3,linewidth=0)
     # plot the adhesion regions
-    mpl.rcParams['hatch.color'] = '0.85'
     plt.axvspan(min(x_full_plot),adhesion_max_nm,color='0.95',
                 linewidth=0)
     PlotUtilities.lazyLabel("Extension (nm)","Force (pN)","")   
     PlotUtilities.x_label_on_top(ax_example)
+    PlotUtilities.no_x_label(ax_example)
+    PlotUtilities.no_y_label(ax_example)
+    x_kwargs = dict(unit_kwargs=dict(fmt="{:.0f}"),width=15,unit="nm")
+    y_kwargs = dict(unit_kwargs=dict(fmt="{:.0f}"),
+                    height=40,unit="pN")
+    Scalebar.crossed_x_and_y_relative(offset_x=0.22,offset_y=0.58,
+                                      x_kwargs=x_kwargs,
+                                      y_kwargs=y_kwargs,
+                                      ax=ax_example)    
     # # plot all the zoomed regions 
-    offsets_x = [0.9,0.5]
-    offsets_y = [0.7,0.15]
-    heights_pN = [5,10]
+    offsets_x = [0.85]
+    offsets_y = [0.65]
+    heights_pN = [10]
     widths_s = [0.001]
     for i,(r,color) in enumerate(zip(regions,colors_regions)):
         ax_tmp = plt.subplot(data_spec[-1])
         dict_tmp = dict(**dict_plot)
         dict_tmp['style_data']['color'] = color[-1]
-        FEC_Plot._fec_base_plot(r.Time,y_func(r),**dict_tmp)
+        time = r.Time
+        force = y_func(r)
+        FEC_Plot._fec_base_plot(time,force,**dict_tmp)
+        # add eyeballed lines to the data
+        slope_1 = 10/3e-3
+        slope_2 = 10/3.6e-3
+        offset_1 = force[0] + 1 
+        offset_2 = force[0] - 16.5
+        line_upper = (time - min(time)) * slope_1 + offset_1
+        line_lower = (time - min(time)) * slope_2 + offset_2
+        time_rel = time - min(time)
+        # dont let the line cross the textbox
+        x_upper_idx = np.where(time_rel < 0.67 * max(time_rel))
+        plt.plot(time[x_upper_idx],line_upper[x_upper_idx],'b--')
+        plt.plot(time,line_lower,'b--')
         xlim = plt.xlim()
         ylim = plt.ylim()
+        # add a little to the x and xlims, so the scale bar has a natural place
+        # place to go 
+        y_range = abs(np.diff(ylim)) 
+        x_range = abs(np.diff(xlim)) 
+        ylim = [ylim[0],ylim[1]+y_range*0.05]
+        xlim = [xlim[0],xlim[1]+x_range*0.1]        
+        ax_tmp.set_ylim(ylim)
+        ax_tmp.set_xlim(xlim)
         min_x = min(xlim)
         max_y = max(ylim)
         y_loc = max_y*0.9
-        line_kw = dict(linewidth=1.0,color='k')
-        x_kwargs =dict(unit="ms",width=widths_s[i],line_kwargs=line_kw,
+        x_kwargs =dict(unit="ms",width=widths_s[i],
                        unit_kwargs=dict(value_function=lambda x: x * 1e3))
-        y_kwargs = dict(unit="pN ",line_kwargs=line_kw,
+        y_kwargs = dict(unit="pN ",
                         height=heights_pN[i])
         offset_x = Scalebar.rel_to_abs(ax_tmp,offsets_x[i],True)
         offset_y = Scalebar.rel_to_abs(ax_tmp,offsets_y[i],False)
@@ -181,10 +206,10 @@ def run():
     fig4d = figure_recreation.save_output(base_re,"Fig1D.csv")      
     ylim = [0,160]
     xlim = [18,32]
-    for x,y in zip(fig4d.x,fig4d.y):
-        plt.plot(x,y,alpha=1,linewidth=0.5)
     for wlc_x,wlc_y in zip(fig4d.wlc_x,fig4d.wlc_y):
-        plt.plot(wlc_x,wlc_y,'b--',alpha=0.4,linewidth=1,zorder=0,dashes=(2,2))
+        plt.plot(wlc_x,wlc_y,'b--',alpha=0.4,linewidth=1,dashes=(2,2))
+    for x,y in zip(fig4d.x,fig4d.y):
+        plt.plot(x,y,alpha=1,linewidth=0.5)        
     ax_fec_ensemble.set_ylim(ylim)
     ax_fec_ensemble.set_xlim(xlim)    
     PlotUtilities.lazyLabel("Extension","Force","")        
@@ -229,6 +254,9 @@ def run():
     # # figure 4C from the science paper -- the pfold energy landscape 
     fig4c = figure_recreation.save_output(base_re,"Fig4C.csv")
     ax_equil = plt.subplot(top_spec[1,2])
+    # data is in kJ/mol, communication with hao, 2017-9-14
+    fig4c.energy /= 4.2
+    fig4c.energy_error /= 4.2
     plt.errorbar(fig4c.x,fig4c.energy,fig4c.energy_error,color=color_equil,
                  marker='o',
                  mfc='w',zorder=0,markerfacecolor="None",capsize=2,elinewidth=1,
@@ -237,8 +265,9 @@ def run():
     x_kwargs = dict(unit_kwargs=dict(fmt="{:.1f}"),width=0.1,unit="nm")
     y_font = copy.deepcopy(Scalebar.def_font_kwargs_y)
     y_font['rotation'] = 90
-    y_kwargs = dict(height=2,unit="kcal/mol",font_kwargs=y_font)
-    Scalebar.crossed_x_and_y_relative(offset_x=0.25,offset_y=0.65,
+    y_kwargs = dict(unit_kwargs=dict(fmt="{:.1f}"),
+                    height=0.5,unit="kcal/mol",font_kwargs=y_font)
+    Scalebar.crossed_x_and_y_relative(offset_x=0.22,offset_y=0.60,
                                       x_kwargs=x_kwargs,
                                       y_kwargs=y_kwargs,
                                       ax=ax_equil)
@@ -255,13 +284,15 @@ def run():
     t = ax_equil.annotate(s=r"$\Delta x^{\ddag}_{\mathrm{Bell}}$",
                           xy=(0.4,0.3),color=color_bell,
                           xycoords="axes fraction")
-    # XXX figure out what is wrong with this?
-    PlotUtilities.no_y_label(ax=ax_equil)                                          
-    loc = [ [0.15,1.15],
-            [-0.05,1.15],
-            [-0.05,1.15]]
+    PlotUtilities.no_y_label(ax=ax_equil)    
+    loc_upper = [-0.05,1.05]
+    loc_lower = [-0.05,0.95]
+    loc = [loc_upper,loc_upper,loc_upper,   
+           loc_lower,loc_lower,loc_lower]
     PlotUtilities.label_tom(fig,loc=loc)
-    PlotUtilities.save_png_and_svg(fig,"diagram")
+    subplots_adjust = dict(hspace=0.05,wspace=0.12)
+    PlotUtilities.save_png_and_svg(fig,"diagram",
+                                   subplots_adjust=subplots_adjust)
     
     
 if __name__ == "__main__":
