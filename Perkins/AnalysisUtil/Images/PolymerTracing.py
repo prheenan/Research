minor bug fixes, got rid of some debugging stuff. looking good, still not sure how to interpret lower persistence length. If 3D has a factor of 2/3, then we are OK...

Perkins/AnalysisUtil/Images/PolymerTracing.py
@@ -383,15 +383,15 @@ def ensemble_polymer_info(objs_all,min_m=0,max_m=np.inf,n_bins=100):
     L_and_angles = [o._L_and_cos_angle(subset=o.dna_subset) for o in objs_all]
     # concatenate all the angles and L0
     L,angles = sorted_concatenated_x_and_y_lists(L_and_angles)
-    L_binned_m,mean_angle_binned = \
+    L_binned,mean_angle_binned = \
         get_L_and_mean_angle(angles, L, n_bins=n_bins,min_cos_angle=0)
-    good_idx = np.where( (L_binned_m >= min_m) & (L_binned_m <= max_m))
-    L_binned = L_binned_m[good_idx]
+    good_idx = np.where( (L_binned >= min_m) & (L_binned <= max_m))
+    L_binned = L_binned[good_idx]
     mean_angle_binned = mean_angle_binned[good_idx]
     Lp_m, log_mean_angle,coeffs = \
         Lp_log_mean_angle_and_coeffs(L_binned, mean_angle_binned)
     polymer_info_obj = polymer_info(L_m=L, cos_angle=angles,
-                                    Lp_m=Lp_m,L0_m=None,L_binned = L_binned_m,
+                                    Lp_m=Lp_m,L0_m=None,L_binned = L_binned,
                                     cos_angle_binned=mean_angle_binned,
                                     coeffs = coeffs)
     return polymer_info_obj

Perkins/Projects/DNA/protein-interactions/prc2/2017-8-1-image-generation/analysis/main_image_analysis.py
@@ -108,18 +108,13 @@ def run():
         im = o.image.height_nm_rel()
         assert (im.shape[0] == im.shape[1])
         assert (im.shape[0] == size_images_pixels)
-    polymer_info_obj = PolymerTracing.ensemble_polymer_info(objs_all)
+    kw = dict(min_m = 25e-9,max_m = 70e-9)
+    polymer_info_obj = PolymerTracing.ensemble_polymer_info(objs_all,**kw)
     L_binned = polymer_info_obj.L_binned
     log_mean_angle = -np.log(polymer_info_obj.cos_angle_binned)
-    plt.plot(L_binned,log_mean_angle)
-    plt.plot(L_binned,np.polyval(polymer_info_obj.coeffs,x=L_binned))
-    plt.show()
     # POST: all the contour lengths are set in 'real' units ]
     L0_protein = np.concatenate([o.L0_protein_dna() for o in objs_all])
     L0_dna = np.concatenate([o.L0_dna_only() for o in objs_all])
-    Lp_dna = np.concatenate([o._f_dna_only(o._Lp) for o in objs_all])
-    print(np.mean(Lp_dna))
-    print(np.std(Lp_dna))
     print_info(L0_dna,"only DNA")
     print_info(L0_protein,"DNA+PRC2")
     n_protein = (L0_protein).size