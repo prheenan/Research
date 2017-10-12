lot s of plot fixes. running. added in offset so that landscapes can all be zeroed, but we can later fit a spline where it is needed. requiring that all points have a well-defined variance. only zeroing A(z) in pedagogy (since others are corrections, seemed appropriate). added in a function that slices and zeros. minor region change

Perkins/Projects/Protein/bacteriorhodopsin/2017-7-jcp-paper/Figures/Energylandscapes/GenerateLandscapes.py
@@ -122,10 +122,10 @@ class landscape_data(object):
         self._extensions_m = np.linspace(min_v,max_v,n*10,endpoint=True)
         # save the origial (ie: same sized grid)
         self._extensions_m_original = np.linspace(min_v,max_v,n,endpoint=True)
-        self._energies = [l.spline_fit.y(self._extensions_m)
+        self._energies = [l.spline_fit.y(self._extensions_m + l.offset)
                           for l in landscape_objs]
-        self._energies_orig = [l.spline_fit.y(self._extensions_m) for l in
-                               original_data]
+        self._energies_orig = [l.spline_fit.y(self._extensions_m + l.offset)
+                               for l in original_data]
         # align all the arbitrary zeros
         self._energies = [e - min(e) for e in self._energies]
         self._energies_orig = [e - min(e) for e in self._energies_orig]
@@ -199,7 +199,7 @@ def grid_interpolate_arrays(x_arr,y_arr,x_grid):
 def _get_landscapes(objects,n_bootstraps,n_bins,cache_dir):
     n = objects[0].Force.size
     n_objs = len(objects)
-    n_min = int(np.ceil(0.8 * n))
+    n_min = n
     for i in range(n_bootstraps):
         # keep looping if > X% of the data is corrupted
         while (True):
@@ -234,14 +234,13 @@ def get_area_bounds(objs,area):
     return to_ret
     
     
-def filter_landscapes(landscapes,n_bins,bins=None,**kw):
+def filter_landscapes(landscapes,n_bins,**kw):
     # zero everything; don't care about absolute offsets.
-    min_v = max([min(l.q) for l in landscapes])
-    max_v = min([max(l.q) for l in landscapes])
-    if (bins is None):
-        bins = np.linspace(min_v,max_v,n_bins)
-    to_ret = [WeierstrassUtil._filter_single_landscape(l,bins,**kw)
-              for l in landscapes]
+    to_ret = []
+    for l in landscapes:
+        bins = np.linspace(min(l.q), max(l.q), n_bins)
+        tmp = WeierstrassUtil._filter_single_landscape(l,bins,**kw)
+        to_ret.append(tmp)
     return to_ret
 
 def heatmap(x,y,bins):                       
@@ -356,14 +355,14 @@ def get_cacheable_data(areas,flickering_dir,cache_dir="./cache"):
     # get a generator (!) for all the raw data...
     data = CheckpointUtilities.multi_load(cache_dir="./raw_data/",
                                           load_func=functor,
-                                          force=False,
+                                          force=False,limit=1,
                                           name_func=name_func)      
     # POST: data_to_use[i] has the data for the spring constant i
     # POST: all data are sorted by spring constant
     data_lengths = []
     filtered_iwt = []
     originals = []
-    N_boostraps = 80
+    N_boostraps = 100
     skip = 0    
     for i,d in enumerate(data):
         data_lengths.append(len(d.data))

Perkins/Projects/Protein/bacteriorhodopsin/2017-7-jcp-paper/Figures/Energylandscapes/main_landscapes.py
@@ -342,9 +342,10 @@ def plot_with_corrections(data):
                 to_second_deriv(f_mean(f_deriv))*convert]
     labels = kwargs_labels()
     kwargs = kwargs_correction()
-    # plot each
+    # plot each, zero out A(z)
+    energies[0] -= np.min(energies[0])
     for i,energy_rel in enumerate(energies):
-        plt.plot(ext_nm,energy_rel-min(energy_rel),label=labels[i],**kwargs[i])
+        plt.plot(ext_nm,energy_rel,label=labels[i],**kwargs[i])
     
 def _second_deriv_plot(ax_heat,data):
     units_y = lambda x: x  * (1e12/1e9)
@@ -535,7 +536,7 @@ def print_info(helical_data):
     ed_mean_delta_per_nm_of_interest = ed_std_delta_per_nm[idx_of_interest]
     mean_ed = ed_std_delta_per_nm_of_interest
     # use SEM / STD?
-    n_bootstraps = 500
+    n_bootstraps = 10
     aa_per_nm = 3
     error_ed = ed_mean_delta_per_nm_of_interest/np.sqrt(n_bootstraps)
     print("The top of the ED helix has energy {:.2f} +/- {:.2g} kcal/mol/aa".\
@@ -557,6 +558,25 @@ def print_info(helical_data):
     print("The min ED energy per nm is {:.2f}/mol".\
           format(min(ed_mean_delta_per_nm)))
 
+def set_landscape_to_slice(landscape,area):
+    offset = min(landscape.q)
+    landscape.q -= offset
+    landscape.offset = offset
+    # determine which indices we want
+    min_v, max_v = area.ext_bounds_nm_rel * 1e-9
+    slice_idx = np.where((landscape.q >= min_v) & \
+                         (landscape.q <= max_v))[0]
+    assert slice_idx.size > 0
+    sanit = lambda x: x[slice_idx].copy()
+    # zero the energy
+    landscape.energy = sanit(landscape.energy)
+    min_e = min(landscape.energy)
+    landscape.energy -= min_e
+    landscape.A_z = sanit(landscape.A_z)
+    landscape.A_z_dot = sanit(landscape.A_z_dot)
+    landscape.one_minus_A_z_ddot_over_k = \
+        sanit(landscape.one_minus_A_z_ddot_over_k)
+    landscape.q = sanit(landscape.q)
 
 def run():
     """
@@ -575,19 +595,13 @@ def run():
     force_recalculation = False
     GenUtilities.ensureDirExists(flickering_dir)
     bin_size_meters = 0.4e-9
-    # write down the areas we want to look at 
-    adhesion_min = 17e-9
-    ed_max = 32e-9
-    cd_max = 48e-9
-    a_max = 70e-9
     slice_area = GenerateLandscapes.slice_area
-    kw = dict(min_v_m=adhesion_min)
-    areas = [\
-        slice_area([adhesion_min,a_max],"Full (no adhesion)",**kw),
-        slice_area([adhesion_min,ed_max],"Helix ED",**kw),
-        slice_area([ed_max,cd_max],"Helix CB",**kw),
-        slice_area([cd_max,a_max],"Helix A",**kw),
-        ]    
+    # convert the regions to nm
+    reg_and_c = jcp_fig_util.regions_and_colors(first_element_is_all=True)
+    regions = [np.array(r[0])*1e-9 for r in reg_and_c]
+    kw = dict(min_v_m=np.min(regions))
+    labels = ["Full (no adhesion)","Helix ED","Helix CB","Helix A"]
+    areas = [slice_area(r,l,**kw) for r,l in zip(regions,labels)]
     for a in areas:
         a.set_num_bins_by_bin_in_meters(bin_size_meters)         
     # read in the data 
@@ -599,23 +613,12 @@ def run():
     helical_data = []
     for a in areas:
         tmp = copy.deepcopy(data_to_analyze)
-        l = copy.deepcopy(tmp.landscape[0][0])
-        l.q -= min(l.q)
-        min_v,max_v = a.ext_bounds_nm_rel*1e-9
-        slice_idx = np.where( (l.q >= min_v) & (l.q <= max_v))[0]
-        assert slice_idx.size > 0
-        sanit = lambda x: x[slice_idx].copy()        
         flat_list = [tmp_landscape for list_v in tmp.landscape 
                      for tmp_landscape in list_v]
         for l in flat_list:
-            l.q = sanit(l.q)
-            # zero the energy
-            l.energy = sanit(l.energy)
-            min_e = min(l.energy)
-            l.energy -=  min_e
-            l.A_z = sanit(l.A_z)
-            l.A_z_dot = sanit(l.A_z_dot)
-            l.one_minus_A_z_ddot_over_k = sanit(l.one_minus_A_z_ddot_over_k)   
+            set_landscape_to_slice(l, a)
+        for o in tmp.originals:
+            set_landscape_to_slice(o, a)
         helical_data.append(tmp)
     # make the pedagogy plot
     make_pedagogical_plot(helical_data[0],landscape_kwargs()[0])