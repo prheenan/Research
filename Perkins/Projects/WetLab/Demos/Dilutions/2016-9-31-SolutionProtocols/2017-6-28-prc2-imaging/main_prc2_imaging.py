7641a8cbc77a898b19e419f6f9a8304ad3afbca6
Your Name <patirck.heenan@colorado.edu>
2017-10-29 14:23:37 -0600

    prc2 imaging

--- Perkins/Projects/WetLab/Demos/Dilutions/2016-9-31-SolutionProtocols/2017-6-28-prc2-imaging/main_prc2_imaging.py
+++ Perkins/Projects/WetLab/Demos/Dilutions/2016-9-31-SolutionProtocols/2017-6-28-prc2-imaging/main_prc2_imaging.py
@@ -61,7 +61,7 @@ def run():
     # stock concentration
     Stock = 1
     # Desired concentrations for dna
-    Volume_total_inc = 15
+    Volume_total_inc = 8
     dna_desired_conc_nM = 20
     dna_desired_pmol = dna_desired_conc_nM*Volume_total_inc
     ratio_prc2_dilution = 4
@@ -69,7 +69,7 @@ def run():
         dna_desired_pmol/(Volume_total_inc)
     # base the PRC2 on it
     Desired_prc2 = ratio_prc2_dilution * dna_imaging_stock_conc_nM/1000 * \
-                   np.array([12] + [1/(4**i) for i in range(-1,3)])
+                   np.array([8,4,2,1])
     num_extra = 1
     # desired volumes (for each)
     volume_1x = Volume_total_inc/ratio_prc2_dilution 
