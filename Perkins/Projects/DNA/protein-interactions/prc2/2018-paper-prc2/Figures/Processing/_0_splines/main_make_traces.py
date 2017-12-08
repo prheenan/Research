53179ab8f4286d80ada7b2d6b28d19ce7229b0cc
patrick heenan <patrick.heenan@colorado.edu>
2017-12-08 15:05:25 -0700

    deleted old processing stuff. added newer stuff, need to be very modular, since there are so many variables...

---
+++ Perkins/Projects/DNA/protein-interactions/prc2/2018-paper-prc2/Figures/Processing/_0_splines/main_make_traces.py
@@ -0,0 +1,28 @@
+# force floating point division. Can still use integer with //
+from __future__ import division
+# other good compatibility recquirements for python3
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+# This file is used for importing the common utilities classes.
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+sys.path.append("../../../../../../../../../../")
+sys.path.append("../../")
+from Util import IoUtil
+
+
+def run(in_dir):
+    """
+    Args:
+        in_dir: the input directory to operate on.  
+    """
+    input_dir =  IoUtil.data_dir(in_dir)
+    print(input_dir)
+    output_dir = IoUtil._traces_dir(in_dir)
+    IoUtil.read_images(input_dir,cache_dir=output_dir)
+    
+if __name__ == "__main__":
+    run(IoUtil.get_directory_command_line())
